9c331dfae14c89797abefd196ea26b807d30eb5a
tsu-nera <fox10225fox@gmail.com>
2019-05-18 10:37:39 +0900

    よくわからん

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,27 +1,8 @@
-const functions = require('firebase-functions');
+import { https } from "firebase-functions"
+import setupGraphQLServer from "./graphql/server"
 
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+/* CF for Firebase with graphql-server-express */
+const graphQLServer = setupGraphQLServer()
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-const app = express();
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+// https://us-central1-<project-name>.cloudfunctions.net/api
+export const api = https.onRequest(graphQLServer)
