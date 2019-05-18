d0eb542cf7eefa74d3d36cd40315640cf6e1d8fe
tsu-nera <fox10225fox@gmail.com>
2019-05-18 22:20:21 +0900

    一旦記録

--- nextjs/pages/index.jsx
+++ nextjs/pages/index.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Query, ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import client from "../lib/apollo";
 
-const Home = () => <div>Hello, World</div>;
+const GET_GROUPS = gql`
+  {
+    groups {
+      name
+      count
+    }
+  }
+`;
+
+const Home = () => (
+  <ApolloProvider client={client}>
+    <Query query={GET_GROUPS}>
+      {({ loading, error, data }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error! ${error.message}`;
+
+        const { groups } = data;
+        return <div>{groups}</div>;
+      }}
+    </Query>
+  </ApolloProvider>
+);
 
 export default Home;
