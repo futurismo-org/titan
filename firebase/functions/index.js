8f938cee3f7054bf6ced1eb3e958b53d6b5f9347
tsu-nera <fox10225fox@gmail.com>
2019-05-18 12:00:12 +0900

    休憩

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,10 +1,8 @@
-import { https } from 'firebase-functions';
-import setupGraphQLServer from './graphql/server';
+const { https } = require('firebase-functions');
+const { setupGraphQLServer } = require('./graphql/server');
 
 /* CF for Firebase with graphql-server-express */
 const graphQLServer = setupGraphQLServer();
 
 // https://us-central1-<project-name>.cloudfunctions.net/
 const api = https.onRequest(graphQLServer);
-
-export default api;
