5a1192a917b0d8589cb3b55e1fc4a127bc6e5939
tsu-nera <fox10225fox@gmail.com>
2019-05-20 00:34:53 +0900

    add post

--- firebase/functions/graphql/data/resolvers.js
+++ firebase/functions/graphql/data/resolvers.js
@@ -8,6 +8,20 @@ const resolveFunctions = {
         .get()
         .then(groups => groups.docs.map(group => group.data()));
     }
+  },
+  Mutation: {
+    addGroup: (headers, req, res) => {
+      const { name } = req;
+      const newGroup = {
+        id: 3,
+        name,
+        count: 0
+      };
+      return db
+        .collection('groups')
+        .add(newGroup)
+        .then(() => newGroup);
+    }
   }
 };
 
