309f7e06820e444cd68c8beb3a7636b130c75cdb
tsu-nera <fox10225fox@gmail.com>
2019-05-22 19:04:27 +0900

    add first components

--- reactjs/src/index.jsx
+++ reactjs/src/index.jsx
@@ -1,1 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import DashBoard from "./components/organisms/DashBoard";
+
+ReactDOM.render(<DashBoard />, document.getElementById('root')); // eslint-disable-line
