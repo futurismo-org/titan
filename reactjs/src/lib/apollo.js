c1ebf6dc2bd6dd09e17a29dfa45caaa68e8dd088
tsu-nera <fox10225fox@gmail.com>
2019-05-25 07:28:40 +0900

    vscode設定

--- reactjs/src/lib/apollo.js
+++ reactjs/src/lib/apollo.js
@@ -1,10 +1,11 @@
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
 import fetch from 'node-fetch';
 
 const API_URL_LOCAL =
   'http://localhost:5000/titan-dev-1234/asia-northeast1/api';
+
 const API_URL_REMOTE =
   'https://asia-northeast1-titan-dev-1234.cloudfunctions.net/api';
 
