251dce117b4a7ded9b7bec5f3c34c3235a470e5f
tsu-nera <fox10225fox@gmail.com>
2019-05-24 09:57:26 +0900

    refactoring

--- reactjs/src/components/organisms/Challenges.jsx
+++ reactjs/src/components/organisms/Challenges.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "react-apollo-hooks";
 import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Hidden from "@material-ui/core/Hidden";
-import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 
 import theme from "../../lib/theme";
+import ChallengeCard from "../atoms/ChallengeCard";
 
 const GET_CHALLENGES = gql`
   {
@@ -28,22 +23,6 @@ const StyledCardGrid = styled(Grid)`
   }
 `;
 
-const StyledCard = styled(Card)`
-  && {
-    display: flex;
-  }
-`;
-
-const StyledCardDetails = styled.div`
-  flex: 1;
-`;
-
-const StyledCardMedia = styled(CardMedia)`
-  && {
-    width: 160px;
-  }
-`;
-
 const Challenges = props => {
   const { data, error, loading } = useQuery(GET_CHALLENGES);
 
@@ -58,34 +37,7 @@ const Challenges = props => {
     <React.Fragment>
       <StyledCardGrid container spacing={4}>
         {data.challenges.map(challenge => (
-          <Grid item key={challenge.title} xs={12} md={6}>
-            <CardActionArea component="a" href="#">
-              <StyledCard>
-                <StyledCardDetails>
-                  <CardContent>
-                    <Typography component="h2" variant="h5">
-                      {challenge.title}
-                    </Typography>
-                    {/* <Typography variant="subtitle1" color="textSecondary">
-                          {challeng.date}
-                        </Typography> */}
-                    <Typography variant="subtitle1" paragraph>
-                      {challenge.discription}
-                    </Typography>
-                    <Typography variant="subtitle1" color="primary">
-                      もっと読む...
-                    </Typography>
-                  </CardContent>
-                </StyledCardDetails>
-                <Hidden xsDown>
-                  <StyledCardMedia
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                </Hidden>
-              </StyledCard>
-            </CardActionArea>
-          </Grid>
+          <ChallengeCard challenge={challenge} key={challenge.id} />
         ))}
       </StyledCardGrid>
     </React.Fragment>
