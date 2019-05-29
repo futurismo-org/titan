dd8aa59aca325fd01252ced8efc349d4937ac920
tsu-nera <fox10225fox@gmail.com>
2019-05-29 16:21:20 +0900

    リファクタリング成功

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 reactjs: cd reactjs && yarn dev
 react-tsc: cd reactjs && yarn watch
-# firebase: cd firebase/functions && yarn dev
+firebase: cd firebase/functions && yarn dev
 # firebase-tsc: cd firebase/functions && yarn watch
--- reactjs/src/components/organisms/Challenges.tsx
+++ reactjs/src/components/organisms/Challenges.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
-import theme from '../../lib/theme';
 import ChallengeCard from '../atoms/ChallengeCard';
 
-import { useChallengesQuery } from '../../gen/graphql-client-api';
+import firebase from '../../lib/firebase';
+import theme from '../../lib/theme';
 
 interface Props {
   container?: any;
@@ -19,24 +20,21 @@ const StyledCardGrid = styled(Grid as React.SFC<Props>)`
 `;
 
 const Challenges = () => {
-  const { data, error, loading } = useChallengesQuery();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error! {error.message}</div>;
-  }
-
-  const challenges = data && data.challenges;
+  const [value, loading, error] = useCollection(
+    firebase.firestore().collection('challenges')
+  );
 
   return (
     <React.Fragment>
-      <StyledCardGrid container spacing={4}>
-        {challenges!.map((challenge: any) => (
-          <ChallengeCard challenge={challenge} key={challenge.id} />
-        ))}
-      </StyledCardGrid>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {value && (
+        <StyledCardGrid container spacing={4}>
+          {value!.docs.map((doc: any) => (
+            <ChallengeCard challenge={doc.data()} key={doc.id} />
+          ))}
+        </StyledCardGrid>
+      )}
     </React.Fragment>
   );
 };
