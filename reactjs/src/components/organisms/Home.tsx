b55c73ba9f960d306e906a7355a317939c4b3066
tsu-nera <fox10225fox@gmail.com>
2019-05-29 14:07:43 +0900

    既存の機能を削除

--- reactjs/src/components/organisms/Home.tsx
+++ reactjs/src/components/organisms/Home.tsx
@@ -5,14 +5,12 @@ import Challenge from './Challenge';
 import Challenges from './Challenges';
 import DashBoard from './DashBoard';
 import Layout from '../templates/DefaultLayout';
-import Singin from './Signin';
 import Categories from './Categories';
 import Category from './Category';
 
 const Home = () => (
   <Layout>
     <Switch>
-      <Route path="/signin" component={Singin} />
       <Route path="/challenges/:id" component={Challenge} />
       <Route path="/challenges" component={Challenges} />
       <Route path="/categories/:id" component={Category} />
