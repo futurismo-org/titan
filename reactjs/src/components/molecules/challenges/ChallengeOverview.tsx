7d4c95b01083df40728ad29924718abbe9a0a34e
tsu-nera <fox10225fox@gmail.com>
2019-05-30 20:05:21 +0900

    show text with whitespace

--- reactjs/src/components/molecules/challenges/ChallengeOverview.tsx
+++ reactjs/src/components/molecules/challenges/ChallengeOverview.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
-const ChallengeOverview = (props: any) => <p>{props.text}</p>;
+const ChallengeOverview = (props: any) => (
+  <div style={{ whiteSpace: 'pre-line' }}>{props.text}</div>
+);
 
 export default ChallengeOverview;
--- reactjs/src/components/molecules/challenges/ChallengeRules.tsx
+++ reactjs/src/components/molecules/challenges/ChallengeRules.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
-const ChallengeRules = (props: any) => <p>{props.text}</p>;
+const ChallengeRules = (props: any) => (
+  <div style={{ whiteSpace: 'pre-line' }}>{props.text}</div>
+);
 
 export default ChallengeRules;
