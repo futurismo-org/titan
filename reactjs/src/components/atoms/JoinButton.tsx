72b900424afdbad40222a9c6461fa9e7d103e6a4
tsu-nera <fox10225fox@gmail.com>
2019-06-01 14:46:49 +0900

    タスク追加

--- reactjs/src/components/atoms/JoinButton.tsx
+++ reactjs/src/components/atoms/JoinButton.tsx
@@ -1,38 +1,20 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import { useCollection } from 'react-firebase-hooks/firestore';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import firebase from '../../lib/firebase';
 
-const createId = (userId: string, challengeId: string) =>
-  `${userId}_${challengeId}`;
-
 const joinHandler = (props: any) => {
   const { challengeId, userId } = props;
-  const id = createId(userId, challengeId);
-  const newData = {
-    id,
-    userId,
-    challengeId,
-    createdAt: new Date()
-  };
+  const userUnion = firebase.firestore.FieldValue.arrayUnion(userId);
   firebase
     .firestore()
-    .collection('user_challenge_relations')
-    .doc(id)
-    .set(newData);
-};
-
-const leaveHandler = (props: any) => {
-  const { challengeId, userId } = props;
-  const id = createId(userId, challengeId);
-  firebase
-    .firestore()
-    .collection('user_challenge_relations')
-    .doc(id)
-    .delete();
+    .collection('challenges')
+    .doc(challengeId)
+    .update({
+      participants: userUnion
+    });
 };
 
 const renderJoinButton = (props: any) => (
@@ -43,17 +25,6 @@ const renderJoinButton = (props: any) => (
     onClick={() => joinHandler(props)}
   >
     参加する
-  </Button>
-);
-
-const renderLeaveButton = (props: any) => (
-  <Button
-    color="inherit"
-    variant="outlined"
-    size="small"
-    onClick={() => leaveHandler(props)}
-  >
-    参加中
   </Button>
 );
 
@@ -74,38 +45,28 @@ const renderPostButton = (props: any) => (
 );
 
 const JoinButton = (props: any) => {
+  const isJoin = (args: {
+    participants: string[];
+    userId: string;
+  }): boolean => {
+    return args.participants && args.participants.includes(args.userId);
+  };
+
   const { challengeId, userId } = props;
   const [join, setJoin] = useState(false);
 
-  const [value, loading, error] = useCollection(
-    firebase.firestore().collection('user_challenge_relations')
-  );
+  firebase
+    .firestore()
+    .collection('challenges')
+    .doc(challengeId)
+    .get()
+    .then((doc: any) => {
+      setJoin(isJoin({ participants: doc.data().participants, userId }));
+    });
 
-  if (error) {
-    return <strong>Error: {error}</strong>;
-  }
-
-  if (loading) {
-    return <div />;
-  }
-
-  value &&
-    challengeId &&
-    userId &&
-    value.query
-      .where('challengeId', '==', challengeId)
-      .where('userId', '==', userId)
-      .get()
-      .then((snapshot: any) => setJoin(!snapshot.empty));
-
-  return join ? (
-    <React.Fragment>
-      {renderLeaveButton({ userId, challengeId })}
-      {renderPostButton({ id: challengeId })}
-    </React.Fragment>
-  ) : (
-    renderJoinButton({ userId, challengeId })
-  );
+  return join
+    ? renderPostButton({ id: challengeId })
+    : renderJoinButton({ userId, challengeId });
 };
 
 const mapStateToProps = (state: any, props: {}) => ({
