a6c980693a61f745fe39410559f273a822dfd13f
tsu-nera <fox10225fox@gmail.com>
2019-06-02 09:53:10 +0900

    wip

--- reactjs/src/components/molecules/challenges/ChallengePosts.tsx
+++ reactjs/src/components/molecules/challenges/ChallengePosts.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import moment, { updateLocale } from 'moment';
+import moment from 'moment';
 import { connect } from 'react-redux';
+import { useDocument } from 'react-firebase-hooks/firestore';
+import firebase from '../../../lib/firebase';
+
 import Record from './ChallengePostRecord';
 import RecordButton from '../../atoms/ChallengeRecordButton';
-import firebase from '../../../lib/firebase';
 
 const StyledCenterContainer = styled.div`
   margin-top: 80px;
@@ -21,27 +23,41 @@ const StyledTimerButtonContainer = styled.div`
 `;
 
 const ChallengePosts = (props: any) => {
-  const [startDate, setStartDate] = useState('');
-  const [days, setDays] = useState(0);
   const { userId, challengeId } = props;
-
   const resourceId = `challenges/${challengeId}/participants/${userId}`;
 
+  const [value, loading, error] = useDocument(
+    firebase.firestore().doc(resourceId)
+  );
+
+  const now = new Date();
+  const initDate = new Date(0);
+
+  const [startDate, setStartDate] = useState(initDate);
+  const [days, setDays] = useState(0);
+  const [updatedAt, setUpdatedAt] = useState(initDate);
+
   const writeRecord = () => {
-    const updateDays = days + 1;
-    const updateDate = days === 0 ? new Date().toISOString() : startDate;
+    if (updatedAt !== initDate && moment(updatedAt).isSame(now, 'day')) {
+      window.alert('記録の投稿は1日１回までです。'); // eslint-disable-line
+      return;
+    }
+
+    const _days = days + 1;
+    const _startDate = days === 0 ? now : startDate;
 
     firebase
       .firestore()
       .doc(resourceId)
       .update({
-        days: updateDays,
-        startDate: updateDate,
-        updatedAt: new Date()
+        days: _days,
+        startDate: _startDate,
+        updatedAt: now
       })
       .then(() => {
-        setDays(updateDays);
-        setStartDate(updateDate);
+        setDays(_days);
+        setStartDate(_startDate);
+        setUpdatedAt(now);
       });
   };
 
@@ -51,12 +67,13 @@ const ChallengePosts = (props: any) => {
       .doc(resourceId)
       .update({
         days: 0,
-        startDate: '',
-        updatedAt: new Date()
+        startDate: now,
+        updatedAt: now
       })
       .then(() => {
         setDays(0);
-        setStartDate('');
+        setStartDate(now);
+        setUpdatedAt(now);
       });
   };
 
@@ -66,25 +83,58 @@ const ChallengePosts = (props: any) => {
     }
   };
 
-  const formatDate = (datetime: string) => {
-    if (datetime !== '') {
-      return moment(datetime).format('YYYY年MM月DD日 HH:mm');
+  const formatDate = (day: Date): string => {
+    if (
+      days === 0 ||
+      (day === undefined || moment(day).isSame(initDate, 'day'))
+    ) {
+      return 'なし';
     }
-    return 'なし';
+    return moment(day).format('YYYY年MM月DD日 HH:mm');
   };
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .doc(resourceId)
+      .get()
+      .then((doc: any) => doc.data())
+      .then((data: any) => {
+        if (data === undefined || data.days === undefined) {
+          setStartDate(initDate);
+          setDays(0);
+          setUpdatedAt(initDate);
+        } else {
+          console.log(data);
+          setStartDate(data.startDate);
+          setDays(data.days);
+          setUpdatedAt(data.updatedAt);
+        }
+      });
+  }, [initDate, resourceId]);
 
   return (
     <StyledCenterContainer>
-      <Record days={days} />
-      <h3>開始日: {formatDate(startDate)}</h3>
-      <StyledTimerButtonContainer>
-        <RecordButton
-          text="記録する"
-          color="primary"
-          handleClick={writeRecord}
-        />
-        <RecordButton text="リセット" color="secondary" handleClick={confirm} />
-      </StyledTimerButtonContainer>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {value && (
+        <React.Fragment>
+          <Record days={days} />
+          <h3>開始日: {formatDate(startDate)}</h3>
+          <StyledTimerButtonContainer>
+            <RecordButton
+              text="記録する"
+              color="primary"
+              handleClick={writeRecord}
+            />
+            <RecordButton
+              text="リセット"
+              color="secondary"
+              handleClick={confirm}
+            />
+          </StyledTimerButtonContainer>
+        </React.Fragment>
+      )}
     </StyledCenterContainer>
   );
 };
