934c28e5d5a00ac3f339229fc12d80b41679e064
tsu-nera <fox10225fox@gmail.com>
2019-06-03 16:53:37 +0900

    generate dummy users

--- firebase/functions/src/cli/seeds.ts
+++ firebase/functions/src/cli/seeds.ts
@@ -27,6 +27,8 @@ const noFapChallengeIds: string[] = [];
 
 const titanUserId = 'MHgvTNT4JrMRKXCmnKbDMZkwv2l2';
 
+const dummyUserIds = [...Array(30).keys()].map((n: number) => ulid());
+
 const createChallengeSeed = (args: any) => {
   const { id } = args;
   return seed.doc(id, {
@@ -84,7 +86,18 @@ const challengeParticipantsSeeds = seed.subcollection([
   createParticipationSeed({
     id: titanUserId,
     histories: [1, 2, 3, 4, 5].map(n => createChallengeHistorySeed(n)),
-    days: 5
+    days: 5,
+    score: 5
+  }),
+  ...dummyUserIds.map((id: string) => {
+    createParticipationSeed({
+      id: id,
+      histories: [
+        ...Array(faker.random.number({ min: 3, max: 10 })).keys()
+      ].map((n: number) => createChallengeHistorySeed(n)),
+      days: faker.random.number({ min: 0, max: 30 }),
+      score: faker.random.number({ min: 0, max: 30 })
+    });
   })
 ]);
 
@@ -157,6 +170,14 @@ const userSeeds = seed.collection('users', [
     id: 'hFVDONlKmeV4snOJGKuUQM5yCtp1',
     photoURL:
       'https://pbs.twimg.com/profile_images/947018640947232768/-Gm-dXvn_normal.jpg'
+  }),
+  ...dummyUserIds.map((id: string) => {
+    return createUserSeed({
+      id: id,
+      email: faker.internet.email(),
+      displayName: faker.name.firstName(),
+      photoURL: faker.image.imageUrl()
+    });
   })
 ]);
 
@@ -167,22 +188,9 @@ const createCollection = (seeds: any) => {
 };
 
 export const createCollections = () => {
+  createCollection(userSeeds);
   createCollection(categorySeeds);
   createCollection(challengeSeeds);
-  createCollection(userSeeds);
-};
-
-const deleteCollection = (title: string) => {
-  cli
-    .firestore()
-    .collection(title)
-    .get()
-    .then((query: QuerySnapshot) =>
-      query.docs.map((doc: DocumentSnapshot) => doc.ref.delete())
-    )
-    .catch((e: any) => {
-      console.log('Failed to delete documents: ' + e);
-    });
 };
 
 const deletePathResource = (path: string) => {
