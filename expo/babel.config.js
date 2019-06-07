e3e83ee20ed700eb4ec30fccaa9f2fa87f308e45
tsu-nera <fox10225fox@gmail.com>
2019-06-07 21:51:53 +0900

    休憩

--- expo/babel.config.js
+++ expo/babel.config.js
@@ -2,17 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['babel-plugin-styled-components'],
-    resolvers: {
-      sourceExts: [
-        'jsx',
-        'js',
-        'json',
-        'ts',
-        'tsx'
-      ],
-    }  
-  };
+    plugins: ['babel-plugin-styled-components', 
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          root: ['./src'],
+        },
+    ]]
+  }
 };
-
-
