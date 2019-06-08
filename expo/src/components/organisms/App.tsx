72f8c52c6ddec084916351d15d122ef3cd3ed960
tsu-nera <fox10225fox@gmail.com>
2019-06-08 11:26:25 +0900

    サンプル動いたの巻

--- expo/src/components/organisms/App.tsx
+++ expo/src/components/organisms/App.tsx
@@ -1,34 +1,105 @@
 import * as React from 'react';
-import styled from 'styled-components/native';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Link } from 'react-router-native';
 
-import { NativeRouter, Link } from 'react-router-native';
-import { Navigation, Card } from 'react-router-navigation';
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    padding: 10
+  },
+  header: {
+    fontSize: 20
+  },
+  nav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  navItem: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  },
+  topic: {
+    textAlign: 'center',
+    fontSize: 15
+  }
+});
 
-import Constants from 'expo-constants';
+const Home = () => {
+  return <Text style={styles.header}>Home</Text>;
+};
 
-const Container = styled(View)`
-  flex: 1px;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
-`;
+const About = () => {
+  return <Text style={styles.header}>About</Text>;
+};
 
-const App = () => (
-  <NativeRouter>
-    <Navigation>
-      <Card
+const Topic = (match: any) => {
+  return <Text style={styles.topic}>{match.params.topicId}</Text>;
+};
+
+const Topics = (match: any) => {
+  return (
+    <View>
+      <Text style={styles.header}>Topics</Text>
+      <View>
+        <Link
+          to={`${match.url}/rendering`}
+          style={styles.subNavItem}
+          underlayColor="#f0f4f7"
+        >
+          <Text>Rendering with React</Text>
+        </Link>
+        <Link
+          to={`${match.url}/components`}
+          style={styles.subNavItem}
+          underlayColor="#f0f4f7"
+        >
+          <Text>Components</Text>
+        </Link>
+        <Link
+          to={`${match.url}/props-v-state`}
+          style={styles.subNavItem}
+          underlayColor="#f0f4f7"
+        >
+          <Text>Props v. State</Text>
+        </Link>
+      </View>
+
+      <Route path={`${match.url}/:topicId`} component={Topic} />
+      <Route
         exact
-        path="/"
-        render={() => (
-          <Link to="/hello">
-            <Text>Press it</Text>
-          </Link>
-        )}
+        path={match.url}
+        render={() => <Text style={styles.topic}>Please select a topic.</Text>}
       />
-      <Card path="/hello" render={() => <Text>Hello</Text>} />
-    </Navigation>
-  </NativeRouter>
-);
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NativeRouter>
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+            <Text>Home</Text>
+          </Link>
+          <Link to="/about" underlayColor="#f0f4f7" style={styles.navItem}>
+            <Text>About</Text>
+          </Link>
+          <Link to="/topics" underlayColor="#f0f4f7" style={styles.navItem}>
+            <Text>Topics</Text>
+          </Link>
+        </View>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </View>
+    </NativeRouter>
+  );
+};
 
 export default App;
