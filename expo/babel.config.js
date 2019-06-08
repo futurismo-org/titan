da30897348ef184225a7c661ef86f8a0be1db72f
tsu-nera <fox10225fox@gmail.com>
2019-06-08 09:17:46 +0900

    表示

--- expo/babel.config.js
+++ expo/babel.config.js
@@ -2,22 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['babel-plugin-styled-components', 
-      [
-        'module-resolver',
-        {
-          extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.android.js',
-            '.android.tsx',
-            '.ios.js',
-            '.ios.tsx',
-          ],
-          root: ['./src'],
-        },
-    ]]
+    plugins: ['babel-plugin-styled-components']
   }
 };
--- expo/webpack.config.js
+++ expo/webpack.config.js
@@ -4,17 +4,22 @@ const expoConfig = require('@expo/webpack-config');
 
 // Create a loader which can import `.obj` & `.mtl` (popular 3D model files (not popular enough to be part of the default config though... 😏))
 const modelLoaderConfiguration = {
-  test: /\.(obj|mtl)$/,
-  use: {
-    loader: 'file-loader'
-  }
+  test: /\.(js|jsx)$/,
+  loader: 'babel-loader',
+  exclude: '/node_modules/'
+};
+
+const testLoaderConfiguration = {
+  test: /\.mjs$/,
+  include: /node_modules/,
+  type: 'javascript/auto'
 };
 
 // Expo expects a function so we can pass around options.
 module.exports = function(env, argv) {
   return merge(expoConfig(env, argv), {
     module: {
-      rules: [modelLoaderConfiguration]
+      rules: [modelLoaderConfiguration, testLoaderConfiguration]
     }
   });
 };
