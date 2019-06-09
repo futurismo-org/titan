f228345f3960a3b257691f62604bb6011e710b58
tsu-nera <fox10225fox@gmail.com>
2019-06-09 17:15:53 +0900

    改良

--- flutter/native/lib/app.dart
+++ flutter/native/lib/app.dart
@@ -46,10 +46,11 @@ class MyHomePage extends StatelessWidget {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "Hello, World!!",
+              "Hello, Flutter!",
             ),
           ],
         ),
