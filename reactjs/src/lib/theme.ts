f2261776d026ba1a049c0adb1dfbc2efda542b6a
tsu-nera <fox10225fox@gmail.com>
2019-06-10 16:07:05 +0900

    runtime errorを潰した

--- reactjs/src/lib/theme.ts
+++ reactjs/src/lib/theme.ts
@@ -1,7 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import blue from '@material-ui/core/colors/blue';
-import pink from '@material-ui/core/colors/pink';
 
 const theme = createMuiTheme({
   palette: {
