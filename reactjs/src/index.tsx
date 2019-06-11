4ff264f3e0f28395e9cd528d6d2d9b67a548ef79
tsu-nera <fox10225fox@gmail.com>
2019-06-11 16:23:48 +0900

    コピペで完成

--- reactjs/src/index.tsx
+++ reactjs/src/index.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
+import createHistory from 'history/createBrowserHistory';
 import App from './components/organisms/App';
 import Head from './components/templates/Head';
 import { store } from './store';
 
 import * as sw from './service-worker';
 
+const history = createHistory();
+ReactGA.initialize('UA-00000000-1');
+history.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search);
+  console.log(location.pathname);
+});
+
 /* eslint-disable */
 ReactDOM.render(
   <React.Fragment>
     <Head />
-    <Router>
+    <Router history={history}>
       <Provider store={store}>
         <App />
       </Provider>
