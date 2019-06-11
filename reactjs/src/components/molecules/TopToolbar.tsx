ff2d02a6783107074f6243b964517087baa23f0c
tsu-nera <fox10225fox@gmail.com>
2019-06-11 12:24:21 +0900

    minor fix

--- reactjs/src/components/molecules/TopToolbar.tsx
+++ reactjs/src/components/molecules/TopToolbar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Toolbar, { ToolbarProps } from '@material-ui/core/Toolbar';
 import Typography, { TypographyProps } from '@material-ui/core/Typography';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import theme from '../../lib/theme';
 import AuthContainer from '../../containers/authContainer';
 
@@ -20,10 +21,15 @@ const StyledTypography = styled(Typography)`
   }
 ` as React.ComponentType<TypographyProps>;
 
+const NoStyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
+
 const TopToolbar = () => (
   <StyledToolbar>
     <StyledTypography component="h2" variant="h5" noWrap>
-      Titan
+      <NoStyledLink to="/">Titan</NoStyledLink>
     </StyledTypography>
     <AuthContainer />
   </StyledToolbar>
