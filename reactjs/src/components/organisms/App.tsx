434c6354cf31e1c271918d458bfb12bc30702a65
tsu-nera <fox10225fox@gmail.com>
2019-06-13 08:29:56 +0900

    hashrouter react-ga対応

--- reactjs/src/components/organisms/App.tsx
+++ reactjs/src/components/organisms/App.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Admin from './admin/Admin';
 import Home from './Home';
-import AdminRoute from '../atoms/AdminRoute';
+import AdminRoute from '../utils/AdminRoute';
+import { withTracker } from '../utils/withtracker';
 
 const App = () => (
   <React.Fragment>
     <Switch>
       <AdminRoute path="/admin" component={Admin} />
-      <Route path="/" component={Home} />
+      <Route path="/" component={withTracker(Home)} />
     </Switch>
   </React.Fragment>
 );
--- reactjs/src/index.tsx
+++ reactjs/src/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
 import App from './components/organisms/App';
 import Head from './components/templates/Head';
 import { store } from './store';
@@ -12,18 +10,10 @@ import * as sw from './service-worker';
 
 require('dotenv').config();
 
-const GA_TRACKING_ID = 'UA-137986489-3';
-
-const history = createBrowserHistory();
-ReactGA.initialize(GA_TRACKING_ID);
-history.listen((location, action) => {
-  ReactGA.pageview(location.pathname + location.search);
-});
-
 ReactDOM.render(
   <React.Fragment>
     <Head />
-    <Router history={history}>
+    <Router>
       <Provider store={store}>
         <App />
       </Provider>
