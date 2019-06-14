41486f70ff80dee876b8227cd66a95097100d1a3
tsu-nera <fox10225fox@gmail.com>
2019-06-14 12:18:23 +0900

    外枠の整備

--- reactjs/src/components/molecules/categories/CategoryBody.tsx
+++ reactjs/src/components/molecules/categories/CategoryBody.tsx
@@ -1,15 +1,38 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+import Paper, { PaperProps } from '@material-ui/core/Paper';
 import theme from '../../../lib/theme';
 
 const CategoryContent = styled.div`
   padding: ${theme.spacing(2)}px;
 `;
 
+const Title = (props: any) => (
+  <Typography component="h3" variant="h4">
+    {props.text}
+  </Typography>
+);
+
+const StyledPaper = styled(Paper)`
+  && {
+    margin: ${theme.spacing(0)}px;
+    padding: ${theme.spacing(3)}px;
+  }
+` as React.ComponentType<PaperProps>;
+
 const CategoryBody = (props: any) => {
   return (
     <CategoryContent>
-      <p>コンテンツ準備中...</p>
+      <StyledPaper>
+        <Title text="概要" />
+      </StyledPaper>
+      <StyledPaper>
+        <Title text="チャレンジ一覧" />
+      </StyledPaper>
+      <StyledPaper>
+        <Title text="Discordフリートーク" />
+      </StyledPaper>
     </CategoryContent>
   );
 };
