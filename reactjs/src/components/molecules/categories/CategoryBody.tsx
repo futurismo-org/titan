4c37351bb310b668a84bda925056e479372d940c
tsu-nera <fox10225fox@gmail.com>
2019-06-14 12:09:36 +0900

    既存ロジックの削除

--- reactjs/src/components/molecules/categories/CategoryBody.tsx
+++ reactjs/src/components/molecules/categories/CategoryBody.tsx
@@ -1,39 +1,15 @@
 import * as React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import CategoryOverview from './CategoryOverview';
-import CategoryDiscussion from './CategoryDiscussion';
-import CategoryRules from './CategoryRules';
-import CategoryLeaderBoard from './CategoryLeaderBoard';
-
-import { withTracker } from '../../utils/withtracker';
 import theme from '../../../lib/theme';
 
 const CategoryContent = styled.div`
   padding: ${theme.spacing(2)}px;
 `;
 
-const CategoryBody = () => {
+const CategoryBody = (props: any) => {
   return (
     <CategoryContent>
-      <Switch>
-        <Route
-          path="/categories/:id/overview"
-          component={withTracker(CategoryOverview)}
-        />
-        <Route
-          path="/categories/:id/discussion"
-          component={CategoryDiscussion}
-        />
-        <Route
-          path="/categories/:id/rules"
-          component={withTracker(CategoryRules)}
-        />
-        <Route
-          path="/categories/:id/leaderboard"
-          component={CategoryLeaderBoard}
-        />
-      </Switch>
+      <p>コンテンツ準備中...</p>
     </CategoryContent>
   );
 };
