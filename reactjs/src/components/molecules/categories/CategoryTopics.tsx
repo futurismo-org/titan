cac8a97580ec9a844b820b4d12dbdf9fa81bedd9
tsu-nera <fox10225fox@gmail.com>
2019-06-21 20:25:09 +0900

    UI整えた

--- reactjs/src/components/molecules/categories/CategoryTopics.tsx
+++ reactjs/src/components/molecules/categories/CategoryTopics.tsx
@@ -7,12 +7,17 @@ import {
   Button
 } from '@material-ui/core';
 import { useCollection } from 'react-firebase-hooks/firestore';
+import styled from 'styled-components';
 import PostButton from '../../atoms/PostButton';
 
 import NoStyledLink from '../../atoms/NoStyledLink';
 
 import firebase from '../../../lib/firebase';
 import Progress from '../../atoms/CircularProgress';
+
+const TitleWrapper = styled.div`
+  display: flex;
+`;
 
 const CategoryTopics = (props: any) => {
   const { category } = props;
@@ -26,36 +31,41 @@ const CategoryTopics = (props: any) => {
   );
 
   return (
-    <React.Fragment>
-      <Typography variant="h5" component="h5">
-        知見まとめ
-      </Typography>
-      <PostButton to={`/categories/${category.id}/topics/new`} />
+    <List>
+      <ListItem>
+        <ListItemText>
+          <Typography variant="h5" component="h5">
+            知見まとめ
+          </Typography>
+        </ListItemText>
+        <PostButton
+          to={`/categories/${category.id}/topics/new`}
+          type="button"
+        />
+      </ListItem>
+      <ListItem />
       {error && <strong>Error: {error}</strong>}
       {loading && <Progress />}
-      {value && (
-        <List>
-          {value!.docs.map((doc: any) => (
-            <ListItem key={doc.id}>
-              <ListItemText>{doc.data().title}</ListItemText>
-              <NoStyledLink to={`/categories/${doc.id}/edit`}>
-                <Button type="button" color="primary" variant="contained">
-                  編集
-                </Button>
-              </NoStyledLink>
-              <Button
-                type="button"
-                color="secondary"
-                variant="contained"
-                // onClick={() => onDeleteHandler(doc.id)}
-              >
-                削除
+      {value &&
+        value!.docs.map((doc: any) => (
+          <ListItem key={doc.id}>
+            <ListItemText>{doc.data().title}</ListItemText>
+            <NoStyledLink to={`/categories/${doc.id}/edit`}>
+              <Button type="button" color="primary" variant="contained">
+                編集
               </Button>
-            </ListItem>
-          ))}
-        </List>
-      )}
-    </React.Fragment>
+            </NoStyledLink>
+            <Button
+              type="button"
+              color="secondary"
+              variant="contained"
+              // onClick={() => onDeleteHandler(doc.id)}
+            >
+              削除
+            </Button>
+          </ListItem>
+        ))}
+    </List>
   );
 };
 
