04bbdd5bd2cf53b6988ae7cb084be7b29212aa35
tsu-nera <fox10225fox@gmail.com>
2019-06-21 18:28:07 +0900

    投稿フォームの作成

--- reactjs/src/components/molecules/DiscussionForm.tsx
+++ reactjs/src/components/molecules/DiscussionForm.tsx
@@ -1,9 +1,123 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ulid } from 'ulid';
+import { TextField, Button } from '@material-ui/core';
 
 const DiscussionForm = (props: any) => {
   const { type } = props;
 
-  return null;
+  const [title, setTitle] = useState('');
+  const [url, setURL] = useState('');
+  const [text, setText] = useState('');
+
+  const onTitleChange = (e: any) => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+  const onURLChange = (e: any) => {
+    e.preventDefault();
+    setURL(e.target.value);
+  };
+  const onTextChange = (e: any) => {
+    e.preventDefault();
+    setText(e.target.value);
+  };
+
+  const isCreate = props.match === undefined;
+
+  const updateHandler = (e: any) => {
+    e.preventDefault();
+
+    const id = isCreate ? ulid() : props.match.params.id;
+    const newData = {
+      id: id,
+      title: title,
+      url: url,
+      text: text
+    };
+
+    // TODO
+    // firebase
+    //   .firestore()
+    //   .collection('categories')
+    //   .doc(id)
+    //   .set(newData);
+    // window.location.href = '/#/admin'; // eslint-disable-line
+  };
+
+  useEffect(() => {
+    if (!isCreate) {
+      // firebase
+      //   .firestore()
+      //   .collection('categories')
+      //   .doc(props.match.params.id)
+      //   .get()
+      //   .then(doc => doc.data())
+      //   .then(challenge => {
+      //     setTitle(challenge!.title);
+      //     setDescription(challenge!.description);
+      //     setOverview(challenge!.overview);
+      //     setChannelId(challenge!.channelId);
+      //     setChallengeRefs(
+      //       !challenge!.challengeRefs
+      //         ? ''
+      //         : challenge!.challengeRefs
+      //             .map(
+      //               (docRef: firebase.firestore.DocumentReference) =>
+      //                 docRef.path
+      //             )
+      //             .toString()
+      //     );
+      //     setCreatedAt(
+      //       moment(challenge!.createdAt.toDate()).format('YYYY-MM-DD')
+      //     );
+      //   });
+    }
+  }, [isCreate]);
+
+  return (
+    <React.Fragment>
+      <form noValidate onSubmit={updateHandler}>
+        <TextField
+          value={title}
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="title"
+          label="タイトル"
+          name="title"
+          autoFocus
+          onChange={onTitleChange}
+        />
+        <TextField
+          value={url}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          id="url"
+          name="url"
+          label="URL"
+          onChange={onURLChange}
+        />
+        or
+        <TextField
+          value={text}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          id="text"
+          name="text"
+          label="テキスト"
+          rows={6}
+          multiline
+          onChange={onTextChange}
+        />
+        <Button type="submit" fullWidth variant="contained" color="primary">
+          投稿
+        </Button>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default DiscussionForm;
