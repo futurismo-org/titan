cd7b194fa15b9185f8c8dc90e7e207f8ceb89688
tsu-nera <fox10225fox@gmail.com>
2019-06-23 12:11:51 +0900

    データ投入の改善

--- firebase/functions/src/utils/admin.ts
+++ firebase/functions/src/utils/admin.ts
@@ -3,10 +3,10 @@ import { configDev, configProd } from './config';
 const admin = require('firebase-admin');
 
 if (admin.apps.length === 0) {
-  if (process.env.APP_ENV === 'development') {
-    admin.initializeApp(configDev);
-  } else {
+  if (process.env.APP_ENV === 'production') {
     admin.initializeApp(configProd);
+  } else {
+    admin.initializeApp(configDev);
   }
 }
 
