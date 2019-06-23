90d31788762de402fb2b1815e50d99716a25530a
tsu-nera <fox10225fox@gmail.com>
2019-06-23 16:34:04 +0900

    add user ranking

---
+++ reactjs/src/components/atoms/NoStyledExternalLink.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const NoStyledExternalLink = styled.a`
+  text-decoration: none;
+  color: inherit;
+`;
+
+export default NoStyledExternalLink;
--- reactjs/src/components/organisms/Ranking.tsx
+++ reactjs/src/components/organisms/Ranking.tsx
@@ -1,7 +1,86 @@
 import * as React from 'react';
 
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import moment from 'moment';
+import Hidden from '@material-ui/core/Hidden';
+import Paper from '../templates/PaperWrapper';
+import Progress from '../atoms/CircularProgress';
+import Avatar from '../atoms/Avatar';
+import Title from '../atoms/Title';
+import NoStyledExternalLink from '../atoms/NoStyledExternalLink';
+import firebase from '../../lib/firebase';
+
+const ConditionalTableCell = (props: any) => (
+  <Hidden only="xs">
+    <TableCell>{props.children}</TableCell>
+  </Hidden>
+);
+
 const Ranking = (props: any) => {
-  return null;
+  const [value, loading, error] = useCollection(
+    firebase.firestore().collection('users')
+  );
+
+  const LeaderBoardHead = () => (
+    <TableHead>
+      <TableRow>
+        <TableCell>順位</TableCell>
+        <ConditionalTableCell />
+        <TableCell>名前</TableCell>
+        <TableCell>最新</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+
+  return (
+    <Paper>
+      <Title text="ユーザランキング" />
+      <p>ランキング機能は準備中です...</p>
+      <p>データが不十分なため、 アクティブユーザを順に表示しています。</p>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <Progress />}
+      {value && (
+        <Table>
+          <LeaderBoardHead />
+          <TableBody>
+            {value.docs
+              .sort((x: any, y: any) =>
+                moment(y.data().updatedAt.toDate()).diff(
+                  moment(x.data().updatedAt.toDate())
+                )
+              )
+              .map((doc, index) => (
+                <TableRow key={doc.data().id}>
+                  <TableCell component="th" scope="row">
+                    {index + 1}位
+                  </TableCell>
+                  <ConditionalTableCell>
+                    <Avatar src={doc.data().photoURL || ''} />
+                  </ConditionalTableCell>
+                  <TableCell>
+                    <NoStyledExternalLink
+                      href={doc.data().twitterURL || 'https://twitter.com'}
+                    >
+                      {doc.data().displayName || 'Annonymous'}
+                    </NoStyledExternalLink>
+                  </TableCell>
+                  <TableCell>
+                    {moment(doc.data().updatedAt.toDate()).fromNow()}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      )}
+    </Paper>
+  );
 };
 
 export default Ranking;
