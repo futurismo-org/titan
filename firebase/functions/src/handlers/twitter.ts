c85cc6824997234061994c2f0f5c4d344e93ad39
tsu-nera <fox10225fox@gmail.com>
2019-06-24 22:07:56 +0900

    環境変数設定

--- firebase/functions/src/handlers/twitter.ts
+++ firebase/functions/src/handlers/twitter.ts
@@ -2,12 +2,22 @@ const Twitter = require('twitter-lite');
 
 const functions = require('firebase-functions');
 
+const CONSUMER_KEY =
+  process.env.APP_ENV === 'development'
+    ? process.env.TWITTER_CONSUMER_KEY
+    : functions.config().twitter.consumer_key;
+
+const CONSUMER_SECRET =
+  process.env.APP_ENV === 'development'
+    ? process.env.TWITTER_CONSUMER_SECRET
+    : functions.config().twitter.consumer_secret;
+
 exports.postTweet = (req: any, res: any) => {
   const client = new Twitter({
-    // consumer_key: CONSUMER_KEY,  // eslint-disable-line
-    // consumer_secret: CONSUMER_SECRET, // eslint-disable-line
-    // access_token_key: titanAccessTokenKey, // eslint-disable-line
-    // access_token_secret: titanAccessTokenSecret // eslint-disable-line
+    consumer_key: CONSUMER_KEY,  // eslint-disable-line
+    consumer_secret: CONSUMER_SECRET, // eslint-disable-line
+    access_token_key: req.body.accessTokenKey, // eslint-disable-line
+    access_token_secret: req.body.accessTokenSecret // eslint-disable-line
   });
 
   client
