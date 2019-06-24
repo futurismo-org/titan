02f0ef1ed62bbecb2cf2b0c6187fd3b76e54b63b
tsu-nera <fox10225fox@gmail.com>
2019-06-24 21:53:29 +0900

    つぶやけた

--- firebase/functions/src/handlers/twitter.ts
+++ firebase/functions/src/handlers/twitter.ts
@@ -2,20 +2,18 @@ const Twitter = require('twitter-lite');
 
 const functions = require('firebase-functions');
 
-// Costomer API Key
-const CONSUMER_KEY = '';
-const CONSUMER_SECRET = '';
-
 exports.postTweet = (req: any, res: any) => {
   const client = new Twitter({
     // consumer_key: CONSUMER_KEY,  // eslint-disable-line
     // consumer_secret: CONSUMER_SECRET, // eslint-disable-line
-    // access_token_key: TsuneraAccessTokenKey, // eslint-disable-line
-    // access_token_secret: TsuneraAccessTokenSecret // eslint-disable-line
+    // access_token_key: titanAccessTokenKey, // eslint-disable-line
+    // access_token_secret: titanAccessTokenSecret // eslint-disable-line
   });
 
   client
-    .get('account/verify_credentials')
+    .post('statuses/update', {
+      status: req.body.content
+    })
     .then((results: any) => {
       console.log('results', results);
     })
