120bfb1d54ba7d65d3a182dae41edf12991e4fd7
tsu-nera <fox10225fox@gmail.com>
2019-06-26 06:05:22 +0900

    add description

--- reactjs/src/components/templates/Head.tsx
+++ reactjs/src/components/templates/Head.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
 
-import rollbarSnippet from '../../lib/rollbar';
-
 const { PUBLIC_URL } = process.env;
 
 const Head = () => {
@@ -13,10 +11,10 @@ const Head = () => {
       <meta name="theme-color" content="#000000" />
       <link rel="manifest" href={`${PUBLIC_URL}/manifest.json`} />
       <script src="https://js.stripe.com/v3/" />
-      <title>Titan App</title>
+      <title>Titan | 自己変革の火をつけるアプリ</title>
       <meta
-        name="google-site-verification"
-        content="CdxVY4YxFr0MeTi2TyoLcCi0XYf2st5HRz3sxmV2QrI"
+        name="description"
+        content="オナ禁・エロ禁を中心とした、自己変革を支援するアプリです。継続できない、情報不足、自己嫌悪感という３つの課題に取り組みます。"
       />
     </Helmet>
   );
