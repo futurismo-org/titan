da9b13a86b22cc672a181c256810f5f23b62d614
tsu-nera <fox10225fox@gmail.com>
2019-06-26 06:01:35 +0900

    log改善

--- reactjs/src/components/templates/Head.tsx
+++ reactjs/src/components/templates/Head.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
 
+import rollbarSnippet from '../../lib/rollbar';
+
 const { PUBLIC_URL } = process.env;
 
-const Head = () => (
-  <Helmet>
-    <link rel="shortcut icon" href={`${PUBLIC_URL}/favicon.ico`} />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <link rel="manifest" href={`${PUBLIC_URL}/manifest.json`} />
-    <script src="https://js.stripe.com/v3/" />
-    <title>Titan App</title>
-  </Helmet>
-);
+const Head = () => {
+  return (
+    <Helmet>
+      <link rel="shortcut icon" href={`${PUBLIC_URL}/favicon.ico`} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="manifest" href={`${PUBLIC_URL}/manifest.json`} />
+      <script src="https://js.stripe.com/v3/" />
+      <title>Titan App</title>
+      <meta
+        name="google-site-verification"
+        content="CdxVY4YxFr0MeTi2TyoLcCi0XYf2st5HRz3sxmV2QrI"
+      />
+    </Helmet>
+  );
+};
 
 export default Head;
