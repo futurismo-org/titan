bcc9be195e4b9c808348a706fa44ffbb60f8fab9
tsu-nera <fox10225fox@gmail.com>
2019-06-28 16:33:06 +0900

    update

--- reactjs/src/lib/global-styles.ts
+++ reactjs/src/lib/global-styles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`@import url('https://fonts.googleapis.com/css?family=M+PLUS+1p');
 body{
-font-family: 'M PLUS 1p';
+  font-family: 'M PLUS 1p' !important;
 }`;
 
 export default GlobalStyle;
