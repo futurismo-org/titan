0d5ee32891c68824fde722df64699806a971d46f
tsu-nera <fox10225fox@gmail.com>
2019-07-08 20:19:16 +0900

    update-description

--- reactjs/src/constants/appInfo.ts
+++ reactjs/src/constants/appInfo.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = 'Titan';
 export const APP_TITLE = `${APP_NAME} | 自己変革の火をつけるアプリ`;
 export const APP_DESCRIPTION =
-  'オナ禁・エロ禁を中心とした、自己変革を支援するアプリです。継続できない、情報不足、自己嫌悪感という３つの課題に取り組みます。';
+  'オナ禁・エロ禁を中心とした、自分を変える若者のためのアプリです。自己変革を支援します。';
 export const APP_PRODUCTION_URL = 'https://titan-fire.com';
 export const APP_DEVELOPMENT_URL = 'https://titan-dev-1234.firebaseapp.com';
 
