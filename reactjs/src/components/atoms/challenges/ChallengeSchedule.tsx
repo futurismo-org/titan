4c123ebe6d0f14f80a5820ab49d06fe64ef98c64
tsu-nera <fox10225fox@gmail.com>
2019-07-09 13:20:29 +0900

    wip

--- reactjs/src/components/atoms/challenges/ChallengeSchedule.tsx
+++ reactjs/src/components/atoms/challenges/ChallengeSchedule.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 
+import moment from 'moment';
+
+const formatDate = (date: string): string => {
+  return moment(date).format('MM月DD日 HH:mm');
+};
+
 const ChallengeSchedule = (props: any) => {
   const { openedAt, closedAt } = props;
   const [activeStep, setActiveStep] = React.useState(0);
-  const steps = [openedAt.toString(), '-', closedAt.toString()];
+  const steps = [
+    formatDate(openedAt.toString()),
+    '',
+    formatDate(closedAt.toString())
+  ];
 
   return (
     <div>
