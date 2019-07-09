88f8c45db2b19bb509f1f15f3683de8b52867d0b
tsu-nera <fox10225fox@gmail.com>
2019-07-09 14:05:39 +0900

    wip

--- reactjs/src/components/atoms/challenges/ChallengeSchedule.tsx
+++ reactjs/src/components/atoms/challenges/ChallengeSchedule.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
@@ -12,22 +12,35 @@ const formatDate = (date: string): string => {
 const ChallengeSchedule = (props: any) => {
   const { openedAt, closedAt } = props;
   const [activeStep, setActiveStep] = React.useState(0);
-  const steps = [
-    formatDate(openedAt.toString()),
-    '',
-    formatDate(closedAt.toString())
-  ];
+  const steps = [formatDate(openedAt), '', formatDate(closedAt)];
+  const status = ['開催前', '開催中', '開催終了'];
+
+  const now = moment(new Date());
+
+  React.useEffect(() => {
+    if (now.isBefore(moment(openedAt))) {
+      setActiveStep(0);
+    } else if (now.isBefore(moment(closedAt))) {
+      setActiveStep(1);
+    } else {
+      setActiveStep(2);
+    }
+  }, [closedAt, now, openedAt]);
 
   return (
-    <div>
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map(label => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
+    <Stepper activeStep={activeStep} alternativeLabel>
+      {[0, 1, 2].map(n => {
+        return (
+          <Step key={n}>
+            <StepLabel>
+              {status[n]}
+              <br />
+              {steps[n]}
+            </StepLabel>
           </Step>
-        ))}
-      </Stepper>
-    </div>
+        );
+      })}
+    </Stepper>
   );
 };
 
