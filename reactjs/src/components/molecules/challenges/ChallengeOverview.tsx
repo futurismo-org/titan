5a31310416556e1a557250a21122d415a69a2573
tsu-nera <fox10225fox@gmail.com>
2019-07-09 09:55:14 +0900

    refactoring

--- reactjs/src/components/molecules/challenges/ChallengeOverview.tsx
+++ reactjs/src/components/molecules/challenges/ChallengeOverview.tsx
@@ -1,46 +1,13 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import MarkdownView from '../../atoms/MarkdownView';
-
-const VideoWrapper = styled.div`
-  max-width: 853px;
-`;
-
-const Video = styled.div`
-  position: relative;
-  padding-bottom: 56.25%; /* 16:9 */
-  padding-top: 25;
-  height: 0;
-`;
-
-const StyledIframe = styled.iframe`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
-const Youtube = (youtubeId: string) => {
-  return (
-    <VideoWrapper>
-      <Video>
-        <StyledIframe
-          title="yotuube"
-          src={`https://www.youtube.com/embed/${youtubeId}`}
-          frameBorder="0"
-        />
-      </Video>
-    </VideoWrapper>
-  );
-};
+import Youtube from '../../atoms/YoutubeWidget';
 
 const ChallengeOverview = (props: any) => {
   const { text, youtubeId } = props;
 
   return (
     <React.Fragment>
-      {youtubeId && Youtube(youtubeId)}
+      {youtubeId && <Youtube id={youtubeId} />}
       <MarkdownView text={text} />
     </React.Fragment>
   );
