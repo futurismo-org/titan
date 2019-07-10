d49200644d085b2822f20e02df8e7537e156c1b3
tsu-nera <fox10225fox@gmail.com>
2019-07-11 03:22:01 +0900

    typo

--- reactjs/src/components/molecules/Topic.tsx
+++ reactjs/src/components/molecules/Topic.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDocument } from 'react-firebase-hooks/firestore';
 
 import moment from 'moment';
