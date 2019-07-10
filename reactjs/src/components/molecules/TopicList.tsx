c9be899d1b66f9fe0c1e785cbc988f6bd7067164
tsu-nera <fox10225fox@gmail.com>
2019-07-11 02:40:52 +0900

    external-url改善

--- reactjs/src/components/molecules/TopicList.tsx
+++ reactjs/src/components/molecules/TopicList.tsx
@@ -14,6 +14,7 @@ import firebase from '../../lib/firebase';
 import Progress from '../atoms/CircularProgress';
 
 import NoStyledLink from '../atoms/NoStyledLink';
+import NoStyledExternalLink from '../atoms/NoStyledExternalLink';
 
 const TopicList = (props: any) => {
   const { collection, collectionId, limit } = props;
@@ -50,11 +51,17 @@ const TopicList = (props: any) => {
               </ListItemAvatar>
               <ListItemText
                 primary={
-                  <NoStyledLink
-                    to={`/${collectionShort}/${collectionId}/t/${topic.id}`}
-                  >
-                    {topic.title}
-                  </NoStyledLink>
+                  topic.url && topic.text === '' ? (
+                    <NoStyledExternalLink href={topic.url} target="_blank">
+                      {topic.title}
+                    </NoStyledExternalLink>
+                  ) : (
+                    <NoStyledLink
+                      to={`/${collectionShort}/${collectionId}/t/${topic.id}`}
+                    >
+                      {topic.title}
+                    </NoStyledLink>
+                  )
                 }
                 secondary={
                   <React.Fragment>
