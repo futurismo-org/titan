9023284d5ab2fc8269cc747e91d7e73a04fb9884
tsu-nera <fox10225fox@gmail.com>
2019-07-11 22:28:44 +0900

    完成

--- reactjs/src/components/molecules/GeneralTopics.tsx
+++ reactjs/src/components/molecules/GeneralTopics.tsx
@@ -1,29 +1,38 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Topics from './Topics';
 import Topic from './Topic';
 import TopicForm from './TopicForm';
 
+import theme from '../../lib/theme';
+
+const TopicContent = styled.div`
+  padding: ${theme.spacing(2)}px;
+`;
+
 const GeneralTopics = (props: any) => {
   return (
-    <Switch>
-      <Route
-        path="/topics/new"
-        render={props => <TopicForm collection="general" {...props} />}
-      />
-      <Route
-        path="/topics/:topicId/edit"
-        render={props => <TopicForm collection="general" {...props} />}
-      />
-      <Route
-        path="/topics/:topicId"
-        render={props => <Topic collection="general" {...props} />}
-      />
-      <Route
-        path="/topics"
-        render={props => <Topics collection="general" {...props} />}
-      />
-    </Switch>
+    <TopicContent>
+      <Switch>
+        <Route
+          path="/topics/new"
+          render={props => <TopicForm collection="general" {...props} />}
+        />
+        <Route
+          path="/topics/:topicId/edit"
+          render={props => <TopicForm collection="general" {...props} />}
+        />
+        <Route
+          path="/topics/:topicId"
+          render={props => <Topic collection="general" {...props} />}
+        />
+        <Route
+          path="/topics"
+          render={props => <Topics collection="general" {...props} />}
+        />
+      </Switch>
+    </TopicContent>
   );
 };
 
