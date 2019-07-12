110655c7e46a7cd1ffcf20088f73b940c416d3b9
tsu-nera <fox10225fox@gmail.com>
2019-07-12 16:53:17 +0900

    update-script

--- reactjs/bin/upload-script.sh
+++ reactjs/bin/upload-script.sh
@@ -1,13 +1,13 @@
+#!/bin/sh
 version=$(git rev-parse HEAD)
 for filename in ./sourceMaps/*; do
-  sliced=${filename//.\/sourceMaps/""}
-  without_map=${sliced//.map/""}
-  minified_url=https://titan-fire.com/static/js$without_map
-  source_map=@${filename//.\//""}
+  without_map=`basename $filename .map`
+  minified_url=https://titan-fire.com/static/js/$without_map
+  source_map=@sourceMaps/`basename $filename`
 
   curl https://api.rollbar.com/api/1/sourcemap \
-  -F access_token=6e15d054ad6b4bd4ae8e68da0bd39c55 \
-  -F version="$version" \
+  -F access_token=7647d09a575c44beae1f7721366c51fe \
+  -F version=$version \
   -F minified_url=$minified_url \
-  -F source_map="$source_map"
+  -F source_map=$source_map
 done
