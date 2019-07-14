572c2b3ba9a8535cd788f434a8b8c1b351b449b7
tsu-nera <fox10225fox@gmail.com>
2019-07-15 08:49:48 +0900

    wip

--- flutternative/lib/themplates/DefaultLayout.dart
+++ flutternative/lib/themplates/DefaultLayout.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class DefaultLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Titan"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
