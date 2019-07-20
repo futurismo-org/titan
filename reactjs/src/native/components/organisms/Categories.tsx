9c4d54139a63d6eed0f1303849cbf1d126d6cdc8
tsu-nera <fox10225fox@gmail.com>
2019-07-20 16:13:53 +0900

    expo-categories

---
+++ reactjs/src/native/components/organisms/Categories.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Title from '../atoms/Title';
+import Progress from '../atoms/CircularProgress';
+import CollectionCard from '../atoms/CollectionCard';
+
+const Challenges = (props: any) => {
+  const { categories, error, loading, fetchCategories } = props;
+
+  React.useEffect(() => {
+    fetchCategories();
+  }, [fetchCategories]);
+
+  return (
+    <React.Fragment>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <Progress />}
+      <Title text="カテゴリ一覧" />
+      {categories.map((category: any) => (
+        <CollectionCard collection={category} key={category.id} />
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default Challenges;
--- reactjs/src/native/components/organisms/Home.tsx
+++ reactjs/src/native/components/organisms/Home.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-native';
 import DashBoard from '~/native/containers/DashBoardContainer';
 import Challenges from '~/native/containers/ChallengesContainer';
+import Categories from '~/native/containers/CategoriesContainer';
 import Layout from '../templates/DefaultLayout';
 
 const Home = () => (
   <Layout>
     <Switch>
       {/* <Route path="/cat/:id" component={Category} />
-      <Route path="/categories" component={Categories} />
-      <Route path="/c/:id" component={Challenge} />
-      
+      <Route path="/c/:id" component={Challenge} />      
       <Route path="/topics" component={GeneralTopics} />
       <Route path="/ranking" component={Ranking} />
       <Route path="/settings" component={Settings} /> */}
+      <Route path="/categories" component={Categories} />
       <Route path="/challenges" component={Challenges} />
       <Route path="/" component={DashBoard} />
     </Switch>
