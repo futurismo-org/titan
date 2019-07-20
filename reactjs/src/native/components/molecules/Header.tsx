b7e9240b253c946916fc453285bc022e8830a4b2
tsu-nera <fox10225fox@gmail.com>
2019-07-20 11:41:12 +0900

    wip - native-baseの障害対応

--- reactjs/src/native/components/molecules/Header.tsx
+++ reactjs/src/native/components/molecules/Header.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
-import { Header, Title } from 'native-base';
+import { Header, Title, Body, Icon, Left, Button } from 'native-base';
 
-const HeaderWrapper = (props: any) => (
-  <Header>
-    <Title>Titan</Title>
-  </Header>
-);
+const HeaderWrapper = (props: any) => {
+  const { drawerRef } = props;
+  return (
+    <React.Fragment>
+      <Header>
+        {/* <Left>
+          <Ionicons name="menu" />
+        </Left> */}
+        <Body>
+          <Title>Titan</Title>
+        </Body>
+      </Header>
+    </React.Fragment>
+  );
+};
 
 export default HeaderWrapper;
--- reactjs/src/native/components/templates/DefaultLayout.tsx
+++ reactjs/src/native/components/templates/DefaultLayout.tsx
@@ -1,17 +1,33 @@
 import * as React from 'react';
 import { ScrollView } from 'react-native';
-import { StyleProvider, Container, Content } from 'native-base';
+import { StyleProvider, Container, Content, Drawer } from 'native-base';
 
 import Header from '~/native/components/molecules/Header';
 import getTheme from '~/native/native-base-theme/components';
 import material from '~/native/native-base-theme/variables/material';
 
 const DefaultLayout = (props: any) => {
+  const [drawer, setDrawer] = React.useState();
+
+  const closeDrawer = () => {
+    drawer._root.close();
+  };
+
+  const openDrawer = () => {
+    drawer._root.open();
+  };
+
   return (
     <StyleProvider style={getTheme(material as any)}>
       <ScrollView>
+        <Drawer
+          ref={ref => setDrawer(ref)}
+          type="overlay"
+          // content={<SideBar navigator={this.navigator} />}
+          onClose={() => closeDrawer()}
+        />
         <Container>
-          <Header />
+          <Header drawerRef={drawer} />
           <Content padder>{props.children}</Content>
         </Container>
       </ScrollView>
