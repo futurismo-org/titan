5978a6be8eddd6883f536e55ccca0df202fd58fc
tsu-nera <fox10225fox@gmail.com>
2019-07-21 18:01:08 +0900

    authutil削除

--- reactjs/src/lib/native/auth.ts
+++
@@ -1,9 +0,0 @@
-import { store } from '~/native/store';
-
-// const user = store.getState().firebase.profile;
-
-// // これでは判定できない。呼び出してすぐは userはnullのようだ
-// const isLogin = !user.isEmpty && user.isLoaded;
-
-// export const isCurrentUser = (shortId: string) =>
-//   isLogin && user.shortId === shortId;
--- reactjs/src/lib/web/auth.ts
+++
@@ -1,9 +0,0 @@
-import { store } from '~/web/store';
-
-// 使えない
-
-// const user = store.getState().firebase.profile;
-
-// export const isLogin = !user.isEmpty && user.isLoaded;
-// export const isCurrentUser = (shortId: string) =>
-//   isLogin && user.shortId === shortId;
