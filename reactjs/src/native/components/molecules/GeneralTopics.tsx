8ebc64b47bd0ec2157e29f1072792af1172d4c1e
tsu-nera <fox10225fox@gmail.com>
2019-07-21 18:49:36 +0900

    topic-done

--- reactjs/src/native/components/molecules/GeneralTopics.tsx
+++ reactjs/src/native/components/molecules/GeneralTopics.tsx
@@ -2,19 +2,19 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router-native';
 import Topics from '../../containers/TopicsContainer';
 import Topic from '../../containers/TopicContainer';
-// import TopicForm from './TopicForm';
+import TopicForm from '../../containers/TopicFormContainer';
 
 const GeneralTopics = (props: any) => {
   return (
     <Switch>
-      {/* <Route
-        path="/topics/new"
-        render={props => <TopicForm collection="general" {...props} />}
-      />
       <Route
         path="/topics/:topicId/edit"
         render={props => <TopicForm collection="general" {...props} />}
-      /> */}
+      />
+      <Route
+        path="/topics/new"
+        render={props => <TopicForm collection="general" {...props} />}
+      />
       <Route
         path="/topics/:topicId"
         render={props => <Topic collection="general" {...props} />}
--- reactjs/src/native/components/molecules/TopicForm.tsx
+++ reactjs/src/native/components/molecules/TopicForm.tsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { TextField, Button } from '@material-ui/core';
 import { withRouter } from 'react-router-native';
+import {
+  Toast,
+  Form,
+  Button,
+  Text,
+  Item,
+  Label,
+  Input,
+  Textarea
+} from 'native-base';
 
-import firebase from 'lib/firebase';
+import firebase from '~/lib/firebase';
 
 const db = firebase.firestore();
 
@@ -32,83 +41,61 @@ const TopicForm = (props: any) => {
     }
   }, [fetchTopic, isCreate, resourceId, topic]);
 
-  const onTitleChange = (e: any) => {
-    e.preventDefault();
-    setTitle(e.target.value);
-  };
-  const onURLChange = (e: any) => {
-    e.preventDefault();
-    setURL(e.target.value);
-  };
-  const onTextChange = (e: any) => {
-    e.preventDefault();
-    setText(e.target.value);
-  };
+  const successToast = () =>
+    Toast.show({
+      text: '投稿に成功しました！',
+      duration: 3000,
+      onClose: () => history.push(redirectPath)
+    });
 
-  const updateHandler = (e: any) => {
-    e.preventDefault();
+  const errorToast = (message: string) =>
+    Toast.show({
+      text: `投稿に失敗しました。(${message})`,
+      duration: 3000
+    });
 
+  const updateHandler = () => {
     if (isCreate) {
       const newData = { title, url, text, ...props.newData };
       db.doc(resourceId)
         .set(newData)
-        .then(() => window.alert('投稿が完了しました。')) // eslint-disable-line
-        .then(() => history.push(redirectPath))
-        .catch(() => window.alert('エラーが発生しました。')); // eslint-disable-line
+        .then(() => successToast())
+        .catch(error => errorToast(error.message));
     } else {
       const updateData = { title, url, text, ...props.updateData };
       db.doc(resourceId)
         .update(updateData)
-        .then(() => window.alert('更新が完了しました。')) // eslint-disable-line
-        .then(() => history.push(redirectPath))
-        .catch(() => window.alert('エラーが発生しました。')); // eslint-disable-line
+        .then(() => successToast())
+        .catch(error => errorToast(error.message));
     }
   };
 
   return (
-    <React.Fragment>
-      <form noValidate onSubmit={updateHandler}>
-        <TextField
-          value={title}
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="title"
-          label="タイトル"
-          name="title"
-          onChange={onTitleChange}
-        />
-        <TextField
-          value={url}
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="url"
-          name="url"
-          label="URL"
-          onChange={onURLChange}
-        />
-        <p>
-          情報をシェアするときは、URLを入力してください。質問するときは、URLを空にしてください。
-        </p>
-        <TextField
-          value={text}
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="text"
-          name="text"
-          label="テキスト"
-          rows={6}
-          multiline
-          onChange={onTextChange}
-        />
-        <Button type="submit" fullWidth variant="contained" color="primary">
-          投稿
-        </Button>
-      </form>
-    </React.Fragment>
+    <Form>
+      <Item floatingLabel>
+        <Label>タイトル</Label>
+        <Input value={title} onChangeText={text => setTitle(text)} />
+      </Item>
+      <Item floatingLabel>
+        <Label>URL</Label>
+        <Input value={url} onChangeText={text => setURL(text)} />
+      </Item>
+      <Text />
+      <Text>
+        情報をシェアするときは、URLを入力してください。質問するときは、URLを空にしてください。
+      </Text>
+      <Textarea
+        bordered
+        rowSpan={5}
+        placeholder="テキスト"
+        value={text}
+        onChangeText={text => setText(text)}
+      />
+      <Text />
+      <Button full rounded primary onPress={() => updateHandler()}>
+        <Text>投稿</Text>
+      </Button>
+    </Form>
   );
 };
 
