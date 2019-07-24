8a4727d0506bb71fce282bc609fbe45542371394
tsu-nera <fox10225fox@gmail.com>
2019-07-24 17:34:42 +0900

    challenge-histories

--- reactjs/src/native/components/molecules/challenges/ChallengeHistories.tsx
+++ reactjs/src/native/components/molecules/challenges/ChallengeHistories.tsx
@@ -1,37 +1,31 @@
 import * as React from 'react';
+import { Badge, Text } from 'native-base';
 import { formatDatetime } from '~/lib/moment';
 
 const { Table, Row } = require('react-native-table-component');
 
-// const getType = (type: string) => {
-//   if (type === 'RESET') {
-//     return <Chip color="primary" label="リセット" />;
-//   }
-//   return <Chip color="secondary" label="記録" />;
-// };
+const getType = (type: string) => {
+  if (type === 'RESET') {
+    return (
+      <Badge primary>
+        <Text>RESET</Text>
+      </Badge>
+    );
+  }
+  return (
+    <Badge info>
+      <Text>RECORD</Text>
+    </Badge>
+  );
+};
 
-// const HistoryRow = (props: any) => {
-//   const { timestamp, score, type, days, diff } = props.history;
-
-//   const wrapShowS = (x: string) => x || '';
-//   const wrapShowN = (x: string) => x || 0;
-
-//   return (
-//     <TableRow>
-//       <TableCell>
-//         {wrapShowS(formatDate(timestamp.toDate().toISOString()))}
-//       </TableCell>
-//       <TableCell>{wrapShowN(score)}</TableCell>
-//       <ConditionalTableCell>{wrapShowN(days)}</ConditionalTableCell>
-//       <ConditionalTableCell>{wrapShowN(diff)}</ConditionalTableCell>
-//       <TableCell>{getType(type)}</TableCell>
-//     </TableRow>
-//   );
-// };
-
-const tableHead = ['日時', 'スコア', 'タイプ'];
+const tableHead = ['日時', '点数', '連続', '累積', '過去', 'タイプ'];
 const HistoryHead = () => (
-  <Row data={tableHead} style={{ backgroundColor: '#f1f8ff' }} />
+  <Row
+    flexArr={[3, 1, 1, 1, 1, 2]}
+    data={tableHead}
+    style={{ backgroundColor: '#f1f8ff' }}
+  />
 );
 
 const ChallengeHistories = (props: any) => {
@@ -46,9 +40,14 @@ const ChallengeHistories = (props: any) => {
           const rowData = [
             formatDatetime(history.timestamp.toDate()),
             history.score,
-            history.type
+            history.days,
+            history.accDays,
+            history.pastDays,
+            getType(history.type)
           ];
-          return <Row data={rowData} key={history.id} />;
+          return (
+            <Row data={rowData} key={history.id} flexArr={[3, 1, 1, 1, 1, 2]} />
+          );
         })}
     </Table>
   );
