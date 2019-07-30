5729f8da926be8c98e43f32fb9b4eb5f0db1b1f9
tsu-nera <fox10225fox@gmail.com>
2019-07-30 20:06:46 +0900

    titleを追加

--- reactjs/src/native/components/organisms/Info.tsx
+++ reactjs/src/native/components/organisms/Info.tsx
@@ -1,39 +1,45 @@
 import * as React from 'react';
 import { List, ListItem, Text } from 'native-base';
 import { Linking } from 'expo';
+import Title from '../atoms/Title';
 
 const Info = (props: any) => {
   return (
-    <List>
-      <ListItem>
-        <Text
-          onPress={() =>
-            Linking.openURL('https://titan-fire.com/terms_of_use.html')
-          }
-        >
-          利用規約
-        </Text>
-      </ListItem>
-      <ListItem>
-        <Text
-          onPress={() =>
-            Linking.openURL('https://titan-fire.com/privacy_policy.html')
-          }
-        >
-          プライバシーポリシー
-        </Text>
-      </ListItem>
-      <ListItem>
-        <Text onPress={() => Linking.openURL('https://titan-fire.com')}>
-          Titan for Web App
-        </Text>
-      </ListItem>
-      <ListItem>
-        <Text onPress={() => Linking.openURL('https://titan-fire.netlify.com')}>
-          Titan紹介ページ
-        </Text>
-      </ListItem>
-    </List>
+    <React.Fragment>
+      <Title text="関連情報" />
+      <List>
+        <ListItem>
+          <Text
+            onPress={() =>
+              Linking.openURL('https://titan-fire.com/terms_of_use.html')
+            }
+          >
+            利用規約
+          </Text>
+        </ListItem>
+        <ListItem>
+          <Text
+            onPress={() =>
+              Linking.openURL('https://titan-fire.com/privacy_policy.html')
+            }
+          >
+            プライバシーポリシー
+          </Text>
+        </ListItem>
+        <ListItem>
+          <Text onPress={() => Linking.openURL('https://titan-fire.com')}>
+            Titan for Web App
+          </Text>
+        </ListItem>
+        <ListItem>
+          <Text
+            onPress={() => Linking.openURL('https://titan-fire.netlify.com')}
+          >
+            Titan紹介ページ
+          </Text>
+        </ListItem>
+      </List>
+    </React.Fragment>
   );
 };
 
