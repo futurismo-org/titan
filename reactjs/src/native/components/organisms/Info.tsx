24b5140f70b32b0cb95693e885ff0ce76c490f06
tsu-nera <fox10225fox@gmail.com>
2019-07-30 20:02:53 +0900

    外部リンクを貼る

--- reactjs/src/native/components/organisms/Info.tsx
+++ reactjs/src/native/components/organisms/Info.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react';
+import { List, ListItem, Text } from 'native-base';
+import { Linking } from 'expo';
 
 const Info = (props: any) => {
-  return null;
+  return (
+    <List>
+      <ListItem>
+        <Text
+          onPress={() =>
+            Linking.openURL('https://titan-fire.com/terms_of_use.html')
+          }
+        >
+          利用規約
+        </Text>
+      </ListItem>
+      <ListItem>
+        <Text
+          onPress={() =>
+            Linking.openURL('https://titan-fire.com/privacy_policy.html')
+          }
+        >
+          プライバシーポリシー
+        </Text>
+      </ListItem>
+      <ListItem>
+        <Text onPress={() => Linking.openURL('https://titan-fire.com')}>
+          Titan for Web App
+        </Text>
+      </ListItem>
+      <ListItem>
+        <Text onPress={() => Linking.openURL('https://titan-fire.netlify.com')}>
+          Titan紹介ページ
+        </Text>
+      </ListItem>
+    </List>
+  );
 };
 
 export default Info;
