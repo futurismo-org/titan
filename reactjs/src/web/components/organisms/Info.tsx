1cc3b6e5241083742daabd4c54f5103ad53c6ace
tsu-nera <fox10225fox@gmail.com>
2019-07-30 13:25:37 +0900

    faq

--- reactjs/src/web/components/organisms/Info.tsx
+++ reactjs/src/web/components/organisms/Info.tsx
@@ -35,9 +35,6 @@ const Info = (props: any) => {
             <ListItemText>ご意見お問い合わせ</ListItemText>
           </ListItem>
           <ListItem>
-            <ListItemText>FAQ</ListItemText>
-          </ListItem>
-          <ListItem>
             <ListItemText>
               <NoStyledExternalLink href="https://titan-fire.netlify.com">
                 Titan紹介ペーシ
