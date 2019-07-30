37a21c3ad476a6bf1556eb80dcbeb6bfb6999451
tsu-nera <fox10225fox@gmail.com>
2019-07-31 06:54:30 +0900

    minor-fix

--- reactjs/src/lib/challenge.ts
+++ reactjs/src/lib/challenge.ts
@@ -5,7 +5,7 @@ export const challengePeriod = (challenge: any) => {
   const closedAt = moment(challenge.closedAt.toDate());
   const today = moment();
 
-  if (openedAt.diff(today, 'days') > 0) {
+  if (openedAt.diff(today, 'days') >= 0) {
     return `${openedAt.fromNow()}に開始`;
   }
   return `${closedAt.fromNow()}に終了`;
