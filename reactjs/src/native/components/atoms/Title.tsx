54363aa5af4f9125bc916ff5f274da6eedf93217
tsu-nera <fox10225fox@gmail.com>
2019-08-04 07:51:25 +0900

    finish

--- reactjs/src/native/components/atoms/Title.tsx
+++ reactjs/src/native/components/atoms/Title.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { Text } from 'react-native';
+import { human } from 'react-native-typography';
 import styled from 'styled-components';
 
 const StyledText = styled(Text)`
-  font-size: 36px;
   margin-bottom: 12px;
 `;
 
 const Title = (props: any) => {
   const { text } = props;
 
-  return <StyledText>{text}</StyledText>;
+  return <StyledText style={human.largeTitle}>{text}</StyledText>;
 };
 
 export default Title;
