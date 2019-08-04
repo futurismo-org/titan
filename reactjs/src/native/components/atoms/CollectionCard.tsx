73a1b9121b5eed81c9b0d4ced74efdc214483195
tsu-nera <fox10225fox@gmail.com>
2019-08-04 10:32:16 +0900

    原型はできた

--- reactjs/src/native/components/atoms/CollectionCard.tsx
+++ reactjs/src/native/components/atoms/CollectionCard.tsx
@@ -5,14 +5,14 @@ import {
   Image,
   TouchableOpacity,
   StyleSheet,
-  Dimensions,
-  Platform
+  Dimensions
 } from 'react-native';
-import { Text, H2, Card, CardItem } from 'native-base';
+import { Text } from 'native-base';
 import styled from 'styled-components/native';
 import { Link } from 'react-router-native';
-import { human } from 'react-native-typography';
 import { collectionURL, getRandomImageURL } from '~/lib/url';
+
+import { isiOS } from '~/lib/native';
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get(
   'window'
@@ -32,25 +32,92 @@ export const itemWidth = slideWidth + itemHorizontalMargin * 2;
 
 const entryBorderRadius = 8;
 
-const SlideInnerContainer = styled(TouchableOpacity)`
-  width: itemWidth;
-  height: slideHeight;
-  padding-top: itemHorizontalMargin;
-  padding-bottom: 18;
-`;
+const colors = {
+  black: '#1a1917',
+  gray: '#888888',
+  background1: '#B721FF',
+  background2: '#21D4FD'
+};
 
-const Shadow = styled(View)`
-  position: absolute;
-  top: 0;
-  left: itemHorizontalMargin;
-  right: itemHorizontalMargin;
-  bottom: 18;
-  shadowColor: black;
-  shadowOpacity: 0.25;
-  shadowOffset: { width: 0, height: 10 };
-  shadowRadius: 10;
-  borderRadius: entryBorderRadius;
-`;
+const styles = StyleSheet.create({
+  slideInnerContainer: {
+    width: itemWidth,
+    height: slideHeight,
+    paddingHorizontal: itemHorizontalMargin,
+    paddingBottom: 18 // needed for shadow
+  },
+  shadow: {
+    position: 'absolute',
+    top: 0,
+    left: itemHorizontalMargin,
+    right: itemHorizontalMargin,
+    bottom: 18,
+    shadowColor: colors.black,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 10 },
+    shadowRadius: 10,
+    borderRadius: entryBorderRadius
+  },
+  imageContainer: {
+    flex: 1,
+    marginBottom: isiOS ? 0 : -1, // Prevent a random Android rendering issue
+    backgroundColor: 'white',
+    borderTopLeftRadius: entryBorderRadius,
+    borderTopRightRadius: entryBorderRadius
+  },
+  imageContainerEven: {
+    backgroundColor: colors.black
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: 'cover',
+    borderRadius: isiOS ? entryBorderRadius : 0,
+    borderTopLeftRadius: entryBorderRadius,
+    borderTopRightRadius: entryBorderRadius
+  },
+  // image's border radius is buggy on iOS; let's hack it!
+  radiusMask: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: entryBorderRadius,
+    backgroundColor: 'white'
+  },
+  radiusMaskEven: {
+    backgroundColor: colors.black
+  },
+  textContainer: {
+    justifyContent: 'center',
+    paddingTop: 20 - entryBorderRadius,
+    paddingBottom: 20,
+    paddingHorizontal: 16,
+    backgroundColor: 'white',
+    borderBottomLeftRadius: entryBorderRadius,
+    borderBottomRightRadius: entryBorderRadius
+  },
+  textContainerEven: {
+    backgroundColor: colors.black
+  },
+  title: {
+    color: colors.black,
+    fontSize: 13,
+    fontWeight: 'bold',
+    letterSpacing: 0.5
+  },
+  titleEven: {
+    color: 'white'
+  },
+  subtitle: {
+    marginTop: 6,
+    color: colors.gray,
+    fontSize: 12,
+    fontStyle: 'italic'
+  },
+  subtitleEven: {
+    color: 'rgba(255, 255, 255, 0.7)'
+  }
+});
 
 const StyledImage = styled(Image)`
   height: 200px;
@@ -67,39 +134,45 @@ const CollectionCard = (props: any) => {
     <React.Fragment>
       {collection.sensitive && !allowSensitive ? (
         <Link to="/settings">
-          <Card>
-            <CardItem header>
-              <H2 style={{ width: '100%' }}>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.slideInnerContainer}
+          >
+            <View style={styles.shadow} />
+            <View style={styles.textContainer}>
+              <Text style={styles.title} numberOfLines={2}>
                 センシティブな内容が含まれている可能性のあるコンテンツです
-              </H2>
-            </CardItem>
-            <CardItem footer>
-              <Text style={human.headline}>設定を変更</Text>
-            </CardItem>
-          </Card>
+              </Text>
+              <Text style={styles.subtitle} numberOfLines={2}>
+                設定を変更
+              </Text>
+            </View>
+          </TouchableOpacity>
         </Link>
       ) : (
         <Link to={path}>
-          <SlideInnerContainer activeOpacity={1}></SlideInnerContainer>
-          <View />
+          <TouchableOpacity
+            activeOpacity={1}
+            style={styles.slideInnerContainer}
+          >
+            <View style={styles.shadow} />
+            <View style={styles.imageContainer}>
+              <Image
+                source={{ uri: getRandomImageURL() }}
+                style={styles.image}
+              />
+              <View style={styles.radiusMask} />
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.title} numberOfLines={2}>
+                {collection.title}
+              </Text>
+              <Text style={styles.subtitle} numberOfLines={2}>
+                {collection.description}
+              </Text>
+            </View>
+          </TouchableOpacity>
         </Link>
-        // <Link to={path}>
-        //   <Card>
-        //     <CardItem header>
-        //       <Text style={{ width: '100%' } && human.title1}>
-        //         {collection.title}
-        //       </Text>
-        //     </CardItem>
-        //     {!small && (
-        //       <CardItem cardBody>
-        //         <StyledImage source={{ uri: getRandomImageURL() }} />
-        //       </CardItem>
-        //     )}
-        //     <CardItem footer>
-        //       <Text style={human.headline}>{collection.description}</Text>
-        //     </CardItem>
-        //   </Card>
-        // </Link>
       )}
     </React.Fragment>
   );
--- reactjs/src/native/components/organisms/DashBoard.tsx
+++ reactjs/src/native/components/organisms/DashBoard.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Text } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import CollectionCard from '~/native/containers/CollectionCardContainer';
 import Progress from '../atoms/CircularProgress';
 import Title from '../atoms/Title';
@@ -18,10 +19,20 @@ const DashBoard = (props: any) => {
   } = props;
 
   React.useEffect(() => {
-    fetchChallenges(4);
-    fetchCategories(4);
+    fetchChallenges(6);
+    fetchCategories(6);
     fetchPinnedChallenges();
   }, [fetchCategories, fetchChallenges, fetchPinnedChallenges]);
+
+  const _renderChallengeItem = (props: any) => {
+    const { item, index } = props;
+    return <CollectionCard collection={item} type="challenges" key={index} />;
+  };
+
+  const _renderCategoryItem = (props: any) => {
+    const { item, index } = props;
+    return <CollectionCard collection={item} type="categories" key={index} />;
+  };
 
   return (
     <React.Fragment>
@@ -30,31 +41,28 @@ const DashBoard = (props: any) => {
       {!loading && (
         <React.Fragment>
           <Title text="運営からのおすすめ" />
-          {pinned.map((pinned: any) => (
-            <CollectionCard
-              collection={pinned}
-              type="challenges"
-              key={pinned.id}
-            />
-          ))}
+          <Carousel
+            data={pinned}
+            renderItem={_renderChallengeItem}
+            sliderWidth={400}
+            itemWidth={400}
+          />
           <MoreLink to="/challenges" />
           <Title text="人気のカテゴリ" />
-          {categories.map((category: any) => (
-            <CollectionCard
-              collection={category}
-              type="categories"
-              key={category.id}
-            />
-          ))}
+          <Carousel
+            data={categories}
+            renderItem={_renderCategoryItem}
+            sliderWidth={400}
+            itemWidth={400}
+          />
           <MoreLink to="/categories" />
           <Title text="人気のチャレンジ" />
-          {challenges.map((challenge: any) => (
-            <CollectionCard
-              collection={challenge}
-              type="challenges"
-              key={challenge.id}
-            />
-          ))}
+          <Carousel
+            data={challenges}
+            renderItem={_renderChallengeItem}
+            sliderWidth={400}
+            itemWidth={400}
+          />
           <MoreLink to="/challenges" />
         </React.Fragment>
       )}
