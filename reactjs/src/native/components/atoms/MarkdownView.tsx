e61a7b8c8af75cf687ec2714c6651be8d6847841
tsu-nera <fox10225fox@gmail.com>
2019-08-05 11:40:52 +0900

    デプロイの順番を変えた

--- reactjs/src/native/components/atoms/MarkdownView.tsx
+++ reactjs/src/native/components/atoms/MarkdownView.tsx
@@ -1,34 +1,6 @@
 import * as React from 'react';
-import Markdown, { getUniqueID } from 'react-native-markdown-renderer';
-import { Text } from 'native-base';
+import Markdown from 'react-native-markdown-renderer';
 import { View } from 'react-native';
-
-// const rules = {
-//   heading1: (node: any, children: any, parent: any, styles: any) => (
-//     <Text
-//       key={getUniqueID()}
-//       style={[styles.heading, styles.heading1, { paddingTop: padding }]}
-//     >
-//       [{children}]
-//     </Text>
-//   ),
-//   heading2: (node: any, children: any, parent: any, styles: any) => (
-//     <Text
-//       key={getUniqueID()}
-//       style={[styles.heading, styles.heading2, { paddingTop: 10 }]}
-//     >
-//       [{children}]
-//     </Text>
-//   ),
-//   heading3: (node: any, children: any, parent: any, styles: any) => (
-//     <Text
-//       key={getUniqueID()}
-//       style={[styles.heading, styles.heading3, { paddingTop: 10 }]}
-//     >
-//       [{children}]
-//     </Text>
-//   )
-// };
 
 const MarkdownView = (props: any) => (
   <View style={{ flex: 1 }}>
