4695880b10573ac738482c381b838d7276424e20
tsu-nera <fox10225fox@gmail.com>
2019-08-05 11:56:05 +0900

    web-font改善

--- reactjs/src/lib/global-styles.ts
+++ reactjs/src/lib/global-styles.ts
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`@import url('https://fonts.googleapis.com/css?family=M+PLUS+1p');
+const GlobalStyle = createGlobalStyle`@import url("https://fonts.googleapis.com/css?family=M+PLUS+1p:500&display=swap&subset=japanese");
 body{
-  font-family: 'M PLUS 1p' !important;
+  font-family: 'M PLUS 1p', sans-serif !important;
 }`;
 
 export default GlobalStyle;
