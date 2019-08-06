bc38ad53932a4a2ad48541d9e358eca569e4c87c
tsu-nera <fox10225fox@gmail.com>
2019-08-07 04:25:52 +0900

    webviewでyoutubeを再生

--- reactjs/src/native/components/atoms/YoutubeWidget.tsx
+++ reactjs/src/native/components/atoms/YoutubeWidget.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
+import { WebView } from 'react-native';
 
-import { Text } from 'native-base';
-import { Linking } from 'expo';
+import { deviceWidth } from '~/lib/native';
 
 const YoutubeWidget = (props: any) => {
   const youtubeId = props.id;
-  const url = `https://www.youtube.com/watch?v=${youtubeId}`;
+  const url = `https://www.youtube.com/embed/${youtubeId}?autoplay=0&showinfo=0&controls=0`;
+
+  const width = deviceWidth - 20;
+  const height = (width * 9) / 16;
+
   return (
-    <Text
-      style={{ textDecorationLine: 'underline' }}
-      onPress={() => Linking.openURL(url)}
-    >
-      Youtube動画へのリンク
-    </Text>
+    <WebView
+      style={{ width, height, alignSelf: 'center', flex: 1, marginBottom: 10 }}
+      javaScriptEnabled
+      domStorageEnabled
+      source={{ uri: url }}
+    />
   );
 };
 
