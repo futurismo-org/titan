8fef9345311f125e847a4d0ef3e5a1d2f1ccaaff
tsu-nera <fox10225fox@gmail.com>
2019-08-09 08:39:18 +0900

    ダイアログ表示

--- reactjs/src/web/components/organisms/Profile.tsx
+++ reactjs/src/web/components/organisms/Profile.tsx
@@ -1,12 +1,32 @@
 import React, { useEffect } from 'react';
 
 import { Follow } from 'react-twitter-widgets';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Error from '../atoms/Error';
 import Progress from '../atoms/CircularProgress';
 import Title from '../atoms/Title';
 
 import Paper from '../templates/PaperWrapper';
 import UserAvatar from '../atoms/UserAvatar';
+
+import { brandWhite } from '~/lib/theme';
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  right: auto;
+  bottom: auto;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  background: ${brandWhite};
+`;
 
 const Profile = (props: any) => {
   const {
@@ -23,6 +43,25 @@ const Profile = (props: any) => {
     fetchUserWithShortId(userShortId);
   }, [fetchUserWithShortId, userShortId]);
 
+  const [openMute, setOpenMute] = React.useState(false);
+  const [openBlock, setOpenBlock] = React.useState(false);
+
+  const handleOpenBlock = () => {
+    setOpenBlock(true);
+  };
+
+  const handleCloseBlock = () => {
+    setOpenBlock(false);
+  };
+
+  const handleOpenMute = () => {
+    setOpenMute(true);
+  };
+
+  const handleCloseMute = () => {
+    setOpenMute(false);
+  };
+
   return (
     <React.Fragment>
       {error && <Error error={error} />}
@@ -33,9 +72,83 @@ const Profile = (props: any) => {
           <UserAvatar photoURL={user.photoURL} userId={user.shortId} large />
           {!!user.twitterUsername && <Follow username={user.twitterUsername} />}
           <p>コンテンツ準備中...</p>
-          <br />
           {isLogin && !isMyProfile && (
-            <p style={{ textDecoration: 'underline' }}>ミュートする</p>
+            <React.Fragment>
+              <div style={{ marginTop: 20, display: 'flex' }}>
+                <div role="button" onClick={handleOpenMute}>
+                  <p style={{ textDecoration: 'underline' }}>ミュート</p>
+                </div>
+                <div role="button" onClick={handleOpenBlock}>
+                  <p style={{ textDecoration: 'underline', marginLeft: 10 }}>
+                    ブロック
+                  </p>
+                </div>
+              </div>
+              <Dialog
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={openMute}
+                onClose={handleCloseMute}
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {user.displayName}さんをミュートしますか？
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    ミュートすると、そのユーザの投稿は表示されなくなります。
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    onClick={handleCloseMute}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    いいえ
+                  </Button>
+                  <Button
+                    onClick={handleCloseMute}
+                    variant="contained"
+                    color="primary"
+                    autoFocus
+                  >
+                    はい
+                  </Button>
+                </DialogActions>
+              </Dialog>
+              <Dialog
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={openBlock}
+                onClose={handleCloseBlock}
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {user.displayName}さんをブロックしますか？
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    ブロックすると、そのユーザはあなたの投稿がみれなくなります。(現在開発中)
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    onClick={handleCloseBlock}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    いいえ
+                  </Button>
+                  <Button
+                    onClick={handleCloseBlock}
+                    variant="contained"
+                    color="primary"
+                    autoFocus
+                  >
+                    はい
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </React.Fragment>
           )}
         </Paper>
       )}
