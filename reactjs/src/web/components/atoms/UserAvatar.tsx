a8a4bec3d4bbb59bce6f0bbb1bd75eb1cf3764a2
tsu-nera <fox10225fox@gmail.com>
2019-08-08 15:52:30 +0900

    さくっとプロフィール作成

--- reactjs/src/web/components/atoms/UserAvatar.tsx
+++ reactjs/src/web/components/atoms/UserAvatar.tsx
@@ -3,15 +3,23 @@ import { Avatar } from '@material-ui/core';
 import NoStyledLink from './NoStyledLink';
 
 const UserAvatar = (props: any) => {
-  const { photoURL, userId } = props;
+  const { photoURL, userId, large } = props;
 
   const path = userId ? `/u/${userId}` : '/users';
 
   const src = photoURL || 'https://titan-fire.com/anonymous.png';
 
+  const style = large
+    ? {
+        margin: 10,
+        width: 60,
+        height: 60
+      }
+    : {};
+
   return (
     <NoStyledLink to={path}>
-      <Avatar src={src} />
+      <Avatar src={src} style={style} />
     </NoStyledLink>
   );
 };
--- reactjs/src/web/components/organisms/Profile.tsx
+++ reactjs/src/web/components/organisms/Profile.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 
+import { Follow } from 'react-twitter-widgets';
 import Error from '../atoms/Error';
 import Progress from '../atoms/CircularProgress';
 import Title from '../atoms/Title';
+
+import Paper from '../templates/PaperWrapper';
+import UserAvatar from '../atoms/UserAvatar';
 
 const Profile = (props: any) => {
   const { fetchUserWithShortId, userShortId, user, error, loading } = props;
@@ -15,7 +19,14 @@ const Profile = (props: any) => {
     <React.Fragment>
       {error && <Error error={error} />}
       {loading && <Progress />}
-      {user && <Title text={`${user.displayName}さんのプロフィール`} />}
+      {!loading && user && (
+        <Paper>
+          <Title text={`${user.displayName}さんのプロフィール`} />
+          <UserAvatar photoURL={user.photoURL} userId={user.shortId} large />
+          <Follow username={user.twitterUsername} />
+          <p>コンテンツ準備中です...もうしばらくお待ち下さいm(._.)m</p>
+        </Paper>
+      )}
     </React.Fragment>
   );
 };
