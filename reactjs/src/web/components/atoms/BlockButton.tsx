9fa36b13c3590d4434188cab7a48c1d106e108c7
tsu-nera <fox10225fox@gmail.com>
2019-08-09 09:07:43 +0900

    refactoring

---
+++ reactjs/src/web/components/atoms/BlockButton.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const BlockButton = (props: any) => {
+  const { user } = props;
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleUpdate = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <div role="button" onClick={handleOpen}>
+        <p style={{ textDecoration: 'underline', marginLeft: 10 }}>ブロック</p>
+      </div>
+      <Dialog
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle id="alert-dialog-title">
+          {user.displayName}さんをブロックしますか？
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            ブロックすると、そのユーザはあなたの投稿がみれなくなります。
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant="contained" color="secondary">
+            いいえ
+          </Button>
+          <Button
+            onClick={handleUpdate}
+            variant="contained"
+            color="primary"
+            autoFocus
+          >
+            はい
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+};
+
+export default BlockButton;
