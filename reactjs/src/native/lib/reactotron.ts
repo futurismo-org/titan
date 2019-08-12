bb6fe42f52ba29612be25da7d134d2d7406cafd9
tsu-nera <fox10225fox@gmail.com>
2019-08-12 17:21:04 +0900

    wip

--- reactjs/src/native/lib/reactotron.ts
+++ reactjs/src/native/lib/reactotron.ts
@@ -1,7 +1,9 @@
 import Reactotron from 'reactotron-react-native';
 
-// TODO 設定が途中です。。。
-
-Reactotron.configure({ name: 'mobile' })
+Reactotron.configure({
+  name: 'mobile'
+})
   .useReactNative({})
   .connect();
+
+export default Reactotron;
