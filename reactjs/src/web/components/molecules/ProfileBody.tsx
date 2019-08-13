158be7eb8f7b43ce9168feeea0cd2787af22a11f
tsu-nera <fox10225fox@gmail.com>
2019-08-13 21:09:26 +0900

    タイトルを優しい感じにした

--- reactjs/src/web/components/molecules/ProfileBody.tsx
+++ reactjs/src/web/components/molecules/ProfileBody.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Paper from '../templates/PaperWrapper';
+import Title from '../atoms/Title';
 
 const ProfileBody = (props: any) => {
   const { user } = props;
 
   return (
     <Paper>
-      <p>test</p>
+      <Title text="参加中のチャレンジ" />
+      <Title text="参加中のカテゴリ" />
+      <Title text="過去のチャレンジ実績" />
     </Paper>
   );
 };
