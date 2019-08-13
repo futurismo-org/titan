d618d8d51affb8646a3aca6d51c4acc5ab01074b
tsu-nera <fox10225fox@gmail.com>
2019-08-13 20:46:41 +0900

    profileリファクタリング

---
+++ reactjs/src/web/components/molecules/ProfileBody.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileBody = (props: any) => {
+  return (
+    <React.Fragment>
+      <p>test</p>
+    </React.Fragment>
+  );
+};
+
+export default ProfileBody;
--- reactjs/src/web/components/organisms/Profile.tsx
+++ reactjs/src/web/components/organisms/Profile.tsx
@@ -1,39 +1,12 @@
 import React, { useEffect } from 'react';
 
-import { Grid, Icon } from '@material-ui/core';
-import styled from 'styled-components';
-import Paper, { PaperProps } from '@material-ui/core/Paper';
+import Paper from '@material-ui/core/Paper';
 import Error from '../atoms/Error';
 import Progress from '../atoms/CircularProgress';
 import Title from '../atoms/Title';
 
-import UserAvatar from '../atoms/UserAvatar';
-
-import MuteButton from '~/web/containers/MuteButtonContainer';
-import BlockButton from '~/web/containers/BlockButtonContainer';
-import theme, { twitterColor, brandGray } from '~/lib/theme';
-import NoStyledExternalLink from '../atoms/NoStyledExternalLink';
-import { TITAN_TWITTER_URL } from '~/constants/appInfo';
-import PostButton from '../atoms/PostButton';
-import { formatYearDate } from '~/lib/moment';
-import { getRandomImageURL } from '~/lib/url';
-
-const ProfileContent = styled.div`
-  text-align: center;
-`;
-
-const MainFeaturedPost = styled(Paper)`
-  && {
-    position: relative;
-    background-color: ${theme.palette.grey[800]};
-    color: ${theme.palette.common.white};
-    margin-bottom: ${theme.spacing(8)}px;
-    background-image: url(${getRandomImageURL()});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-  }
-` as React.ComponentType<PaperProps>;
+import ProfileHeader from '../molecules/ProfileHeader';
+import ProfileBody from '../molecules/ProfileBody';
 
 const Profile = (props: any) => {
   const {
@@ -69,81 +42,12 @@ const Profile = (props: any) => {
           </Paper>
         ) : (
           <React.Fragment>
-            <MainFeaturedPost>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  transform: 'translate3d(0, 50%, 0)'
-                }}
-              >
-                <UserAvatar
-                  photoURL={user.photoURL}
-                  userId={user.shortId}
-                  xlarge
-                />
-              </div>
-            </MainFeaturedPost>
-            <Grid container justify="center" style={{ padding: 20 }}>
-              <Grid item md={12}>
-                <ProfileContent>
-                  <div>
-                    <h2>{user.displayName}</h2>
-                  </div>
-                  <div>
-                    {!!user.twitterUsername && (
-                      <NoStyledExternalLink href={TITAN_TWITTER_URL}>
-                        <Icon
-                          className="fab fa-twitter"
-                          style={{ color: twitterColor }}
-                        />
-                      </NoStyledExternalLink>
-                    )}
-                  </div>
-                  <div>
-                    {!!user.introduction && (
-                      <div>
-                        <p>{user.introduction}</p>
-                      </div>
-                    )}
-                  </div>
-                  <div>
-                    {isLogin && !isMyProfile && (
-                      <div
-                        style={{
-                          display: 'flex',
-                          justifyContent: 'center'
-                        }}
-                      >
-                        <MuteButton user={user} />
-                        <BlockButton user={user} />
-                      </div>
-                    )}
-                  </div>
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      color: brandGray
-                    }}
-                  >
-                    <p>{`登録日 ${formatYearDate(user.createdAt.toDate())}`}</p>
-                    <p style={{ marginLeft: 10 }}>{`最終更新日 ${formatYearDate(
-                      user.updatedAt.toDate()
-                    )}`}</p>
-                  </div>
-                </ProfileContent>
-              </Grid>
-              <Grid item md={12}>
-                <div style={{ textAlign: 'right' }}>
-                  <PostButton
-                    to="/settings"
-                    type="button"
-                    text="プロフィールを編集"
-                  />
-                </div>
-              </Grid>
-            </Grid>
+            <ProfileHeader
+              user={user}
+              isLogin={isLogin}
+              isMyProfile={isMyProfile}
+            />
+            <ProfileBody />
           </React.Fragment>
         ))}
     </React.Fragment>
