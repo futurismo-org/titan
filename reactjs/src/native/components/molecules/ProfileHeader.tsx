b604ad5efaa688133cf2907eafe1f3e11a21da52
tsu-nera <fox10225fox@gmail.com>
2019-08-15 03:59:24 +0900

    さらにせいり

--- reactjs/src/native/components/molecules/ProfileHeader.tsx
+++ reactjs/src/native/components/molecules/ProfileHeader.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { View, Button } from 'native-base';
+
+import UserAvatar from '../atoms/UserAvatar';
+import { getTwitterProfileURL } from '~/lib/url';
+import TouchableText from '../atoms/TouchableText';
+
+import MuteButton from '~/native/containers/MuteButtonContainer';
+import BlockButton from '~/native/containers/BlockButtonContainer';
 
 const ProfileHeader = (props: any) => {
-  return null;
+  const { user, isLogin, isMyProfile } = props;
+
+  return (
+    <View>
+      <UserAvatar photoURL={user.photoURL} userId={user.shortId} large />
+      {!!user.twitterUsername && (
+        <Button style={{ marginVertical: 15 }} info small>
+          <TouchableText
+            text="Twitter"
+            url={getTwitterProfileURL(user.twitterUsername)}
+            external
+          />
+        </Button>
+      )}
+      {isLogin && !isMyProfile && (
+        <View style={{ marginTop: 20, flex: 1, flexDirection: 'row' }}>
+          <MuteButton user={user} />
+          <View style={{ marginLeft: 10 }}>
+            <BlockButton user={user} />
+          </View>
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default ProfileHeader;
--- reactjs/src/native/components/organisms/Profile.tsx
+++ reactjs/src/native/components/organisms/Profile.tsx
@@ -1,13 +1,7 @@
 import React, { useEffect } from 'react';
-import { Text, View, Button } from 'native-base';
+import { Text, View } from 'native-base';
 import Error from '../atoms/Error';
 import Progress from '../atoms/CircularProgress';
-import UserAvatar from '../atoms/UserAvatar';
-import { getTwitterProfileURL } from '~/lib/url';
-import TouchableText from '../atoms/TouchableText';
-
-import MuteButton from '~/native/containers/MuteButtonContainer';
-import BlockButton from '~/native/containers/BlockButtonContainer';
 import Title from '../atoms/Title';
 
 import ProfileHeader from '../molecules/ProfileHeader';
@@ -53,33 +47,6 @@ const Profile = (props: any) => {
               isMyProfile={isMyProfile}
             />
             <ProfileBody userShortId={userShortId} />
-            <Text
-              style={{
-                marginBottom: 12,
-                marginTop: 6,
-                fontWeight: 'bold',
-                fontSize: 22
-              }}
-            >{`${user.displayName}さんのプロフィール`}</Text>
-            <UserAvatar photoURL={user.photoURL} userId={user.shortId} large />
-            {!!user.twitterUsername && (
-              <Button style={{ marginVertical: 15 }} info small>
-                <TouchableText
-                  text="Twitter"
-                  url={getTwitterProfileURL(user.twitterUsername)}
-                  external
-                />
-              </Button>
-            )}
-            <Text>コンテンツ準備中...</Text>
-            {isLogin && !isMyProfile && (
-              <View style={{ marginTop: 20, flex: 1, flexDirection: 'row' }}>
-                <MuteButton user={user} />
-                <View style={{ marginLeft: 10 }}>
-                  <BlockButton user={user} />
-                </View>
-              </View>
-            )}
           </View>
         ))}
     </React.Fragment>
