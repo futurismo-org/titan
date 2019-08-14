0a3ffc6d66f952a177fbf144544f2afab82ce503
tsu-nera <fox10225fox@gmail.com>
2019-08-15 05:20:18 +0900

    scoreboard

--- reactjs/src/native/components/molecules/ProfileHeader.tsx
+++ reactjs/src/native/components/molecules/ProfileHeader.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text } from 'native-base';
+import { View, Text, Button } from 'native-base';
 
 import { Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { withRouter } from 'react-router-native';
 import UserAvatar from '../atoms/UserAvatar';
 import { getTwitterProfileURL, getRandomImageURL } from '~/lib/url';
 
@@ -10,12 +11,14 @@ import MuteButton from '~/native/containers/MuteButtonContainer';
 import BlockButton from '~/native/containers/BlockButtonContainer';
 import { StyledHero as Hero } from '~/native/components/atoms/Hero';
 
+import TotalScoreBoard from '~/native/containers/TotalScoreBoardContainer';
+
 import { twitterColor, brandGray } from '~/lib/theme';
 
 import { formatYearDate } from '~/lib/moment';
 
 const ProfileHeader = (props: any) => {
-  const { user, isLogin, isMyProfile } = props;
+  const { user, isLogin, isMyProfile, history } = props;
 
   return (
     <View style={{ alignItems: 'center' }}>
@@ -31,6 +34,7 @@ const ProfileHeader = (props: any) => {
           {user.displayName}
         </Text>
       </View>
+      <TotalScoreBoard userShortId={user.shortId} />
       <View>
         <Text style={{ padding: 20 }}>{user.introduction}</Text>
       </View>
@@ -62,8 +66,15 @@ const ProfileHeader = (props: any) => {
           </View>
         </View>
       )}
+      {isLogin && isMyProfile && (
+        <View style={{ marginTop: 20 }}>
+          <Button rounded onPress={() => history.push('/settings')}>
+            <Text>プロフィールを編集</Text>
+          </Button>
+        </View>
+      )}
     </View>
   );
 };
 
-export default ProfileHeader;
+export default withRouter(ProfileHeader);
