b8044fd03fdb909fa223baf7d1547ccd63173c0a
tsu-nera <fox10225fox@gmail.com>
2019-08-18 05:14:40 +0900

    item生成

--- reactjs/src/native/components/molecules/QuickActionList.tsx
+++ reactjs/src/native/components/molecules/QuickActionList.tsx
@@ -1,10 +1,59 @@
 import React from 'react';
-import { View, Text } from 'native-base';
+import {
+  View,
+  Text,
+  Content,
+  Card,
+  CardItem,
+  Button,
+  Grid,
+  Col,
+  Row
+} from 'native-base';
+import Title from '../atoms/Title';
+
+const QuickActionCard = (props: any) => {
+  return (
+    <Card>
+      <Grid>
+        <Row
+          style={{
+            height: 50,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>オナ禁大会1ヶ月チャレンジ</Text>
+        </Row>
+        <Row>
+          <Col>
+            <Button full success>
+              <Text>記録する</Text>
+            </Button>
+          </Col>
+          <Col>
+            <Button full warning>
+              <Text>リセット</Text>
+            </Button>
+          </Col>
+          <Col>
+            <Button full>
+              <Text>カテゴリ</Text>
+            </Button>
+          </Col>
+        </Row>
+      </Grid>
+    </Card>
+  );
+};
 
 const QuickActionList = (props: any) => {
   return (
     <View>
-      <Text>test</Text>
+      <Content padder>
+        <Title text="クイックアクション" />
+        <QuickActionCard />
+      </Content>
     </View>
   );
 };
