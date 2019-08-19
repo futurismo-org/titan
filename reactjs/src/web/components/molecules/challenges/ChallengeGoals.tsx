e18d8558f63062914e7b8b2a084445d40f9c3464
tsu-nera <fox10225fox@gmail.com>
2019-08-19 19:50:36 +0900

    ツールの選定

--- reactjs/src/web/components/molecules/challenges/ChallengeGoals.tsx
+++ reactjs/src/web/components/molecules/challenges/ChallengeGoals.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import StackGrid from 'react-stack-grid';
 
 const ChallengeGoals = (props: any) => {
   return (
     <React.Fragment>
-      <p>ゴールのページです。</p>
+      <StackGrid columnWidth={150}>
+        <div key="key1">Item 1</div>
+        <div key="key2">Item 2</div>
+        <div key="key3">Item 3</div>
+      </StackGrid>
     </React.Fragment>
   );
 };
