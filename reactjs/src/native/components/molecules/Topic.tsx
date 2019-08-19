7c0a4ebd8dc6254e3683c79ddfd62d393a8cd835
tsu-nera <fox10225fox@gmail.com>
2019-08-19 10:48:03 +0900

    レイアウト調整

--- reactjs/src/native/components/molecules/Topic.tsx
+++ reactjs/src/native/components/molecules/Topic.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Button, Text, View } from 'native-base';
+import { Button, Text, View, Content } from 'native-base';
 import { Linking, Alert, TouchableOpacity } from 'react-native';
 import { withRouter, Link } from 'react-router-native';
 import Title from '../atoms/Title';
@@ -73,11 +73,12 @@ const Topic = (props: any) => {
             <Text>設定を変更</Text>
           </TouchableOpacity>
         ) : blocked ? (
-          <React.Fragment>
-            <Title text="表示をブロックしました" />
-            <Text>
-              あなたはこの記事を投稿したユーザからブロックされているため、記事を閲覧できません。
-            </Text>
+            <Content padder>
+              <Title text="表示をブロックしました" />
+              <Text>
+                あなたはこの記事を投稿したユーザからブロックされているため、記事を閲覧できません。
+              </Text>
+            </Content>
           </React.Fragment>
         ) : (
           <React.Fragment>
