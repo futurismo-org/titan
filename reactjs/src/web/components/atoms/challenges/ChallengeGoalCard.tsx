4d08d29550b857221d82734367380def6c84011a
tsu-nera <fox10225fox@gmail.com>
2019-08-19 19:58:05 +0900

    静的にカードを並べてみた

---
+++ reactjs/src/web/components/atoms/challenges/ChallengeGoalCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CardContent, Typography, Card } from '@material-ui/core';
+
+const ChallengeGoalCard = (props: any) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography component="p" variant="caption">
+          tsu-nera
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ChallengeGoalCard;
--- reactjs/src/web/components/molecules/challenges/ChallengeGoals.tsx
+++ reactjs/src/web/components/molecules/challenges/ChallengeGoals.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import StackGrid from 'react-stack-grid';
+import ChallengeGoalCard from '../../atoms/challenges/ChallengeGoalCard';
 
 const ChallengeGoals = (props: any) => {
   return (
     <React.Fragment>
-      <StackGrid columnWidth={150}>
-        <div key="key1">Item 1</div>
-        <div key="key2">Item 2</div>
-        <div key="key3">Item 3</div>
+      <StackGrid columnWidth={200}>
+        <ChallengeGoalCard key={1} />
+        <ChallengeGoalCard key={2} />
+        <ChallengeGoalCard key={3} />
+        <ChallengeGoalCard key={4} />
+        <ChallengeGoalCard key={5} />
+        <ChallengeGoalCard key={6} />
+        <ChallengeGoalCard key={7} />
+        <ChallengeGoalCard key={8} />
+        <ChallengeGoalCard key={9} />
+        <ChallengeGoalCard key={10} />
+        <ChallengeGoalCard key={11} />
+        <ChallengeGoalCard key={12} />
       </StackGrid>
     </React.Fragment>
   );
