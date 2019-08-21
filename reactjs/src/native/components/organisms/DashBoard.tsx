6b1be8fecf8355871a2b462e14c86e7fdb1c7b99
tsu-nera <fox10225fox@gmail.com>
2019-08-21 19:51:35 +0900

    なにも表示するものがないときの対処

--- reactjs/src/native/components/organisms/DashBoard.tsx
+++ reactjs/src/native/components/organisms/DashBoard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { View } from 'react-native';
+import { Text } from 'native-base';
 import CollectionCard from '~/native/containers/CollectionCardContainer';
 import Title from '../atoms/Title';
 import MoreLink from '../atoms/MoreLink';
@@ -46,60 +47,72 @@ const DashBoard = (props: any) => {
       {!loading && (
         <View>
           <Title text="オススメのチャレンジ" />
-          <Carousel
-            ref={(c: any) => setChallengeSliderRef(c)}
-            data={challenges}
-            renderItem={_renderChallengeItem}
-            sliderWidth={deviceWidth}
-            itemWidth={deviceWidth}
-            onSnapToItem={index => setChallengeActiveSlide(index)}
-          />
-          <Pagination
-            dotsLength={challenges.length}
-            activeDotIndex={challengeActiveSlide}
-            containerStyle={{ paddingVertical: 0 }}
-            dotColor="gray"
-            dotStyle={{
-              width: 8,
-              height: 8,
-              borderRadius: 4,
-              marginHorizontal: 8,
-              marginVertical: 8
-            }}
-            inactiveDotColor={brandGray}
-            inactiveDotOpacity={0.4}
-            inactiveDotScale={0.6}
-            carouselRef={challengeSliderRef}
-            tappableDots={!!challengeSliderRef}
-          />
+          {challenges.length !== 0 ? (
+            <React.Fragment>
+              <Carousel
+                ref={(c: any) => setChallengeSliderRef(c)}
+                data={challenges}
+                renderItem={_renderChallengeItem}
+                sliderWidth={deviceWidth}
+                itemWidth={deviceWidth}
+                onSnapToItem={index => setChallengeActiveSlide(index)}
+              />
+              <Pagination
+                dotsLength={challenges.length}
+                activeDotIndex={challengeActiveSlide}
+                containerStyle={{ paddingVertical: 0 }}
+                dotColor="gray"
+                dotStyle={{
+                  width: 8,
+                  height: 8,
+                  borderRadius: 4,
+                  marginHorizontal: 8,
+                  marginVertical: 8
+                }}
+                inactiveDotColor={brandGray}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.6}
+                carouselRef={challengeSliderRef}
+                tappableDots={!!challengeSliderRef}
+              />
+            </React.Fragment>
+          ) : (
+            <Text>現在表示できるコンテンツはありません。</Text>
+          )}
           <MoreLink to="/challenges" />
           <Title text="人気のカテゴリ" />
-          <Carousel
-            ref={(c: any) => setCategorySliderRef(c)}
-            data={categories}
-            renderItem={_renderCategoryItem}
-            sliderWidth={deviceWidth}
-            itemWidth={deviceWidth}
-            onSnapToItem={index => setCategoryActiveSlide(index)}
-          />
-          <Pagination
-            dotsLength={categories.length}
-            activeDotIndex={categoryActiveSlide}
-            containerStyle={{ paddingVertical: 0 }}
-            dotColor="gray"
-            dotStyle={{
-              width: 8,
-              height: 8,
-              borderRadius: 4,
-              marginHorizontal: 8,
-              marginVertical: 8
-            }}
-            inactiveDotColor={brandGray}
-            inactiveDotOpacity={0.4}
-            inactiveDotScale={0.6}
-            carouselRef={categorySliderRef}
-            tappableDots={!!categorySliderRef}
-          />
+          {categories.length !== 0 ? (
+            <React.Fragment>
+              <Carousel
+                ref={(c: any) => setCategorySliderRef(c)}
+                data={categories}
+                renderItem={_renderCategoryItem}
+                sliderWidth={deviceWidth}
+                itemWidth={deviceWidth}
+                onSnapToItem={index => setCategoryActiveSlide(index)}
+              />
+              <Pagination
+                dotsLength={categories.length}
+                activeDotIndex={categoryActiveSlide}
+                containerStyle={{ paddingVertical: 0 }}
+                dotColor="gray"
+                dotStyle={{
+                  width: 8,
+                  height: 8,
+                  borderRadius: 4,
+                  marginHorizontal: 8,
+                  marginVertical: 8
+                }}
+                inactiveDotColor={brandGray}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.6}
+                carouselRef={categorySliderRef}
+                tappableDots={!!categorySliderRef}
+              />
+            </React.Fragment>
+          ) : (
+            <Text>現在表示できるコンテンツはありません。</Text>
+          )}
           <MoreLink to="/categories" />
         </View>
       )}
