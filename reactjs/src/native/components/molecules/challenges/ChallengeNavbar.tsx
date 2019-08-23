c4f3bcf0ba91d3ad64de7f1dbbc1c0156a2762c3
tsu-nera <fox10225fox@gmail.com>
2019-08-23 13:30:46 +0900

    ナビゲーションメニューの調整

--- reactjs/src/native/components/molecules/challenges/ChallengeNavbar.tsx
+++ reactjs/src/native/components/molecules/challenges/ChallengeNavbar.tsx
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import Menu, { MenuItem } from 'react-native-material-menu';
 
-import { Link, withRouter } from 'react-router-native';
+import { withRouter } from 'react-router-native';
 import { Button, Text } from 'native-base';
 import TouchableText from '../../atoms/TouchableText';
+import { getChallengeUserGoalPath } from '~/lib/url';
 
 const ChallengeNavbar = (props: any) => {
   const { challenge, isLogin, userShortId, history } = props;
@@ -62,12 +63,24 @@ const ChallengeNavbar = (props: any) => {
             ダッシュボード
           </MenuItem>
         )}
+        <MenuItem onPress={() => hideMenu(`/c/${challengeId}/goals`)}>
+          ゴールボード
+        </MenuItem>
         <MenuItem onPress={() => hideMenu(`/c/${challengeId}/topics`)}>
           トピック
         </MenuItem>
-        <MenuItem onPress={() => hideMenu(`/c/${challengeId}/goals`)}>
-          ゴール
+        <MenuItem onPress={() => hideMenu(`/c/${challengeId}/timelines`)}>
+          タイムライン
         </MenuItem>
+        {isLogin && (
+          <MenuItem
+            onPress={() =>
+              hideMenu(getChallengeUserGoalPath(challengeId, userShortId))
+            }
+          >
+            努力ノート
+          </MenuItem>
+        )}
         {isLogin && (
           <MenuItem
             onPress={() =>
@@ -81,59 +94,5 @@ const ChallengeNavbar = (props: any) => {
     </View>
   );
 };
-// const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-// const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-//   setAnchorEl(event.currentTarget);
-// };
-
-// const handleClose = () => {
-//   setAnchorEl(null);
-// };
-
-// const NavItem = forwardRef((props: any, ref: any) => (
-//   <MenuItem ref={ref} onClick={handleClose}>
-//     <NoStyledLink to={props.to}>{props.text}</NoStyledLink>
-//   </MenuItem>
-// ));
-
-// return (
-//   <React.Fragment>
-//     <Grid item xs={12}>
-//       <ButtonGroup fullWidth>
-//         <Button>
-//           <NoStyledLink to={`/c/${props.id}/overview`}>概要</NoStyledLink>
-//         </Button>
-//         <Button>
-//           <NoStyledLink to={`/c/${props.id}/rules`}>ルール</NoStyledLink>
-//         </Button>
-//         <Button onClick={handleClick}>その他</Button>
-//       </ButtonGroup>
-//     </Grid>
-//     <Menu
-//       id="simple-menu"
-//       anchorEl={anchorEl}
-//       keepMounted
-//       open={Boolean(anchorEl)}
-//       onClose={handleClose}
-//     >
-//       <NavItem to={`/c/${props.id}/timeline`} text="タイムライン" />
-//       <NavItem to={`/c/${props.id}/topics`} text="トピック" />
-//       <NavItem to={`/c/${props.id}/leaderboard`} text="リーダーボード" />
-//       {props.userId && (
-//         <NavItem
-//           to={`/c/${props.id}/u/${props.userId}`}
-//           text="ダッシュボード"
-//         />
-//       )}
-//       {props.userId && (
-//         <NavItem
-//           to={`/c/${props.id}/u/${props.userId}/settings`}
-//           text="ユーザ設定"
-//         />
-//       )}
-//     </Menu>
-//   </React.Fragment>
-// );
 
 export default withRouter(ChallengeNavbar);
