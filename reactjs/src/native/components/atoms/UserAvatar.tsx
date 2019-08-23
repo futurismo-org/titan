68cac5541464c27924f11c1437b1f88a97048adc
tsu-nera <fox10225fox@gmail.com>
2019-08-23 14:55:53 +0900

    目標を設定していないユーザの一覧

--- reactjs/src/native/components/atoms/UserAvatar.tsx
+++ reactjs/src/native/components/atoms/UserAvatar.tsx
@@ -5,9 +5,17 @@ import { withRouter } from 'react-router-native';
 import { ANONYMOUS_AVATAR_URL } from '~/lib/url';
 
 const Avatar = (props: any) => {
-  const { photoURL, userId, small, large, history, additionalHandler } = props;
+  const {
+    photoURL,
+    userId,
+    small,
+    large,
+    history,
+    additionalHandler,
+    to
+  } = props;
 
-  const path = userId ? `/u/${userId}` : '/users';
+  const path = to ? to : userId ? `/u/${userId}` : '/users';
   const src = photoURL || ANONYMOUS_AVATAR_URL;
 
   const handler = () => {
--- reactjs/src/native/components/molecules/challenges/ChallengeGoals.tsx
+++ reactjs/src/native/components/molecules/challenges/ChallengeGoals.tsx
@@ -1,10 +1,70 @@
-import React from 'react';
-import { Text } from 'native-base';
+import React, { useEffect } from 'react';
+import { Text, Content, View } from 'native-base';
+import Title from '../../atoms/Title';
+
+import Error from '../../atoms/Error';
+import Progress from '../../atoms/CircularProgress';
+import UserAvatar from '../../atoms/UserAvatar';
+import { getChallengeUserGoalPath } from '~/lib/url';
+import ViewRow from '../../atoms/ViewRow';
 
 const ChallengeGoals = (props: any) => {
+  const {
+    fetchParticipants,
+    resourceId,
+    participants,
+    loading,
+    error,
+    challengeId,
+    goals,
+    notSetGoals,
+    fetchChallengeObjectives
+  } = props;
+
+  useEffect(() => {
+    participants.length === 0 && fetchParticipants(resourceId);
+    participants.length !== 0 &&
+      !goals &&
+      fetchChallengeObjectives(participants, challengeId);
+  }, [
+    challengeId,
+    fetchChallengeObjectives,
+    fetchParticipants,
+    goals,
+    participants,
+    resourceId
+  ]);
+
   return (
     <React.Fragment>
-      <Text>Goalsのページです</Text>
+      <Content padder>
+        <Title text="仲間たちのチャレンジ目標" />
+        {error && <Error error={error} />}
+        {loading ? <Progress /> : null}
+        <Text />
+        <Text>目標をまだ設定していないユーザ</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            marginTop: 5,
+            padding: 5
+          }}
+        >
+          {!loading &&
+            !!notSetGoals &&
+            notSetGoals.map((user: any) => {
+              return (
+                <UserAvatar
+                  key={user.id}
+                  photoURL={user.photoURL}
+                  userId={user.id}
+                  to={getChallengeUserGoalPath(challengeId, user.id)}
+                />
+              );
+            })}
+        </View>
+      </Content>
     </React.Fragment>
   );
 };
