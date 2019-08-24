7627570d0faec175ed84a78e9adbf04b2b8e84bf
tsu-nera <fox10225fox@gmail.com>
2019-08-24 15:57:04 +0900

    ライブラリの使いかたを修正

--- reactjs/src/connects/UsersConnect.tsx
+++ reactjs/src/connects/UsersConnect.tsx
@@ -7,32 +7,31 @@ const mapStateToProps = (state: any, props: any) => {
   const profiles = state.firestore.ordered.myProfiles;
   const myId = state.firebase.profile.shortId;
 
-  const marged =
-    isLoaded(users) && isLoaded(profiles)
-      ? users
-          .filter((user: any) => !user.freezed)
-          .map((user: any) => {
-            const profile = profiles.filter(
-              (profile: any) => profile.id === user.shortId
-            );
+  const marged = isLoaded(users, profiles)
+    ? users
+        .filter((user: any) => !user.freezed)
+        .map((user: any) => {
+          const profile = profiles.filter(
+            (profile: any) => profile.id === user.shortId
+          );
 
-            const totalScore =
-              profile && profile.length === 1
-                ? (profile as any[])[0].totalScore
-                : 0;
+          const totalScore =
+            profile && profile.length === 1
+              ? (profile as any[])[0].totalScore
+              : 0;
 
-            return {
-              ...user,
-              totalScore
-            };
-          })
-      : [];
+          return {
+            ...user,
+            totalScore
+          };
+        })
+    : [];
 
   console.log(marged, isLoaded(users));
 
   return {
     users: marged,
-    isLoaded: isLoaded([users, profiles]) && !isEmpty([users, profiles]),
+    isLoaded: isLoaded(users, profiles) && !isEmpty(users, profiles),
     myId
   };
 };
