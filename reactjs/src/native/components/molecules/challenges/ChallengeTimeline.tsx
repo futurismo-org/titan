f6d6cee7787b3054c76015d1789625b5c18ced1d
tsu-nera <fox10225fox@gmail.com>
2019-09-02 16:23:38 +0900

    timeineの土台

--- reactjs/src/native/components/molecules/challenges/ChallengeTimeline.tsx
+++ reactjs/src/native/components/molecules/challenges/ChallengeTimeline.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { Text } from 'native-base';
+import React, { useState, useEffect } from 'react';
+import { StreamApp, FlatFeed } from 'expo-activity-feed';
+import { getToken, GETSTREAM_KEY, GETSTREAM_APP_ID } from '~/lib/getstream';
+import Progress from '~/native/components/atoms/CircularProgress';
 
 const ChallengeTimeline = (props: any) => {
+  const { challengeId } = props;
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    getToken(challengeId).then((res: any) => setToken(res));
+  }, [challengeId]);
+
   return (
     <React.Fragment>
-      <Text>タイムラインのページです。</Text>
+      {token === '' && <Progress />}
+      {token !== '' && (
+        <StreamApp
+          apiKey={GETSTREAM_KEY}
+          appId={GETSTREAM_APP_ID}
+          token={token}
+          userId={challengeId}
+          options={{ browser: true }} /* hack */
+        >
+          <FlatFeed />
+        </StreamApp>
+      )}
     </React.Fragment>
   );
 };
