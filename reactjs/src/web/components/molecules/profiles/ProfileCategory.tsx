37a45956e844a2e92dab967f11a7704db7ce3bca
tsu-nera <fox10225fox@gmail.com>
2019-09-11 04:13:17 +0900

    土台をつくった

--- reactjs/src/web/components/molecules/profiles/ProfileCategory.tsx
+++ reactjs/src/web/components/molecules/profiles/ProfileCategory.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CATEGORY_KIND_GOOD } from '~/lib/category';
-// import ProfileCategoryGood from '~/web/containers/ProfileCategoryGoodContainer';
+import ProfileCategoryGood from '~/web/containers/ProfileCategoryGoodContainer';
 import ProfileCategoryBad from '~/web/containers/ProfileCategoryBadContainer';
 
 const ProfileCategory = (props: any) => {
@@ -10,7 +10,9 @@ const ProfileCategory = (props: any) => {
     <React.Fragment>
       {loading && null}
       {!loading &&
-        (category.kind === CATEGORY_KIND_GOOD ? null : ( // <ProfileCategoryGood category={category} userShortId={userShortId} />
+        (category.kind === CATEGORY_KIND_GOOD ? (
+          <ProfileCategoryGood category={category} userShortId={userShortId} />
+        ) : (
           <ProfileCategoryBad category={category} userShortId={userShortId} />
         ))}
     </React.Fragment>
