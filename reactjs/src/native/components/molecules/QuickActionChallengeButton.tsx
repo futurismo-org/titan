4f089a13d813d2146bcc4c601af98f901079158d
tsu-nera <fox10225fox@gmail.com>
2019-09-14 18:55:12 +0900

    hotfix

--- reactjs/src/native/components/molecules/QuickActionChallengeButton.tsx
+++ reactjs/src/native/components/molecules/QuickActionChallengeButton.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Col, Button } from 'native-base';
 import { withRouter } from 'react-router-native';
 import { useDocument } from 'react-firebase-hooks/firestore';
+import Modal from 'react-native-modal';
 import { isPostPossible } from '~/lib/challenge';
 import Error from '../atoms/Error';
+import { RECORD_OPTION_TIME } from '~/constants/strategy';
+import ChallengePostRecordModalTimeForm from './challenges/ChallengePostRecordModalTimeForm';
 
 const QuickActionChallengeButton = (props: any) => {
   const {
@@ -12,7 +15,8 @@ const QuickActionChallengeButton = (props: any) => {
     history,
     showGiphy,
     participantsRef,
-    recordStrategy
+    recordStrategy,
+    recordOption
   } = props;
 
   const [value, loading, error] = useDocument(participantsRef);
@@ -21,28 +25,54 @@ const QuickActionChallengeButton = (props: any) => {
   const resetRecord =
     !!resetHandler && resetHandler(history.push, () => showGiphy('lose'));
 
-  const challenge = value && value.data();
+  const data = value && value.data();
   const recordDisabled =
-    challenge && !isPostPossible(challenge.histories, recordStrategy);
+    data && !isPostPossible(data.histories, recordStrategy);
+
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const ChallengePostRecordModalForm = (props: any) => {
+    const { option } = props;
+
+    let component = null;
+
+    if (option === RECORD_OPTION_TIME) {
+      component = (
+        <ChallengePostRecordModalTimeForm
+          closeHandler={closeModal}
+          postHandler={writeRecord}
+          data={data}
+        />
+      );
+    }
+
+    return component;
+  };
 
   return (
     <React.Fragment>
       {error && <Error error={error} />}
-      {!loading && challenge && (
+      {!loading && data && (
         <React.Fragment>
           <Col>
-            <Button
-              full
-              success
-              onPress={() => writeRecord(challenge)}
-              disabled={recordDisabled}
-            >
+            <Button full success onPress={openModal} disabled={recordDisabled}>
               <Text>記録する</Text>
             </Button>
+            <Modal isVisible={modal} avoidKeyboard>
+              <ChallengePostRecordModalForm option={recordOption} />
+            </Modal>
           </Col>
           {!!resetHandler && (
             <Col>
-              <Button full warning onPress={() => resetRecord(challenge)}>
+              <Button full warning onPress={() => resetRecord(data)}>
                 <Text>リセット</Text>
               </Button>
             </Col>
