78f1d4719d5b932cf19b63acacbb80cafc275728
tsu-nera <fox10225fox@gmail.com>
2019-09-16 18:32:20 +0900

    一覧表示

--- reactjs/src/web/components/organisms/Reviews.tsx
+++ reactjs/src/web/components/organisms/Reviews.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ListItem, ListItemText } from '@material-ui/core';
+import { ListItem, ListItemText, Divider } from '@material-ui/core';
 import Title from '~/web/components/atoms/Title';
 import PostButton from '../atoms/PostButton';
+import Progress from '../atoms/CircularProgress';
 
 const Reviews = (props: any) => {
   const { userShortId, userReviews, loading } = props;
@@ -18,6 +19,20 @@ const Reviews = (props: any) => {
           text="レビューを新規投稿"
         />
       </ListItem>
+      <Divider />
+      <React.Fragment>
+        {loading && <Progress />}
+        {!loading &&
+          userReviews &&
+          userReviews.map((userReview: any) => (
+            <React.Fragment key={userReview.id}>
+              <ListItem>
+                <ListItemText>{userReview.title}</ListItemText>
+              </ListItem>
+              <Divider />
+            </React.Fragment>
+          ))}
+      </React.Fragment>
     </React.Fragment>
   );
 };
