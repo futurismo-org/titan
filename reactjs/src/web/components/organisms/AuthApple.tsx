72287775cf7415b8dbe0922cb2c789b0e8ad3fb9
tsu-nera <fox10225fox@gmail.com>
2019-09-26 20:41:12 +0900

    完成した

--- reactjs/src/web/components/organisms/AuthApple.tsx
+++ reactjs/src/web/components/organisms/AuthApple.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
 import firebase, { createCustomToken } from '~/lib/firebase';
 import connect from '~/connects/AuthConnect';
 
 const jwt = require('jsonwebtoken');
 
 const AuthApple = (props: any) => {
-  const { signInSuccessWithAuthResult } = props;
+  const { signInSuccessWithAuthResult, history } = props;
 
-  const url = location.href; // eslint-disable-line
-  const idToken = url.split('&id_token=')[1];
-  const uid = jwt.decode(idToken).sub;
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    const url = location.href; // eslint-disable-line
+    const idToken = url.split('&id_token=')[1];
+    const uid = jwt.decode(idToken).sub;
 
-  if (!uid) return null;
-
-  createCustomToken(uid, false)
-    .then((token: string) => firebase.auth().signInWithCustomToken(token))
-    .then(credential => signInSuccessWithAuthResult(credential))
-    .then(() => (window.location.href = '/')); // eslint-disable-line
+    !!uid &&
+      createCustomToken(uid, false)
+        .then(token => firebase.auth().signInWithCustomToken(token))
+        .then(credential => signInSuccessWithAuthResult(credential))
+        .then(() => window.alert('AppleIDでログインしました。')) // eslint-disable-line
+        .then(() => history.push('/'));
+  }, []);
+  /* eslint-enable */
 
   return null;
 };
 
-export default connect(AuthApple);
+export default withRouter(connect(AuthApple));
