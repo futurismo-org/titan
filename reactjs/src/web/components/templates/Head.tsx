2038d152fe06dafeedae40508bb741c11ae2a676
tsu-nera <fox10225fox@gmail.com>
2019-10-08 14:42:23 +0900

    fix-build-error

--- reactjs/src/web/components/templates/Head.tsx
+++ reactjs/src/web/components/templates/Head.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import * as AppInfo from 'constants/appInfo';
 
